411fe31a79b9f2831a0a78af739664375c67e782
Dominik Arominski <dominik.arominski@cern.ch>
2023-07-14 11:23:37 +0200

    Extracted background task code to own file

---
+++ background.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <cstring>
+
+#include "addressRegistry.h"
+
+namespace bkgTask
+{
+    /**
+     * Copies all contents of the currently used buffer to the background buffer to synchronise them.
+     */
+    void synchroniseReadBuffers()
+    {
+        auto const& addrRegistrySize = addressRegistry::AddressRegistry::instance().getReadBufferSize();
+        auto const& addressRegistry  = addressRegistry::AddressRegistry::instance().getBufferAddrArray();
+        for (auto iter = 2 * (bufferSwitch ^ 1); iter < addrRegistrySize; iter += 2)
+        {
+            memcpy(
+                reinterpret_cast<void*>(addressRegistry[iter + bufferSwitch ^ 1].m_addr),
+                reinterpret_cast<void*>(addressRegistry[iter + bufferSwitch].m_addr),
+                sizeof(reinterpret_cast<void*>(addressRegistry[iter + bufferSwitch].m_addr))
+                // TODO: better way to get a size of this memory block, currently fetching void* pointer size
+            );
+        }
+    }
+
+    /**
+     * Copies all contents of a write buffer to the background buffer, which is not currently used.
+     */
+    void copyWriteBuffer()
+    {
+        auto const& addrRegistrySize    = addressRegistry::AddressRegistry::instance().getWriteBufferSize();
+        auto const& writeBufferRegistry = addressRegistry::AddressRegistry::instance().getWriteAddrArray();
+        auto const& bkgBufferRegistry   = addressRegistry::AddressRegistry::instance().getBufferAddrArray();
+
+        for (auto iter = 0; iter < addrRegistrySize; iter++)
+        {
+            auto const& targetBufferAddr
+                = bkgBufferRegistry[2 * iter + (bufferSwitch ^ 1)].m_addr;    // only bkg buffer elements are modified
+            auto const& writeBufferAddr = writeBufferRegistry[iter].m_addr;   // each write buffer element is visited
+            memcpy(
+                reinterpret_cast<void*>(targetBufferAddr), reinterpret_cast<void*>(writeBufferAddr),
+                sizeof(reinterpret_cast<void*>(writeBufferAddr))
+                // TODO: better way to get a size of this memory block, currently fetching void* pointer size
+            );
+        }
+    }
+}
