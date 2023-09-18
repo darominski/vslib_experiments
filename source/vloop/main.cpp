6d026475458eaf8d501c666007a1c39b2303657c
Dominik Arominski <dominik.arominski@cern.ch>
2023-09-18 13:41:13 +0200

    Prototype writes manifest to std output

--- source/vloop/main.cpp
+++ source/vloop/main.cpp
@@ -1,10 +1,10 @@
 #include <algorithm>
 #include <array>
+#include <bmboot/payload_runtime.hpp>
 #include <cerrno>
 #include <cstring>
 #include <fcntl.h>
 #include <iostream>
-// #include <sys/mman.h>
 #include <unistd.h>
 
 #include "background.h"
@@ -26,39 +26,11 @@ using namespace vslib;
 
 int main()
 {
-    // // Create shared memory region
-    // const char* memory_address      = "/shared_mem";
-    // int         shared_memory_field = shm_open(memory_address, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
-    // if (shared_memory_field == -1)
-    // {
-    //     utils::LogString error_msg("ERROR: Failed to create shared memory.\n");
-    //     std::cerr << error_msg;
-    //     return 1;
-    // }
+    bmboot::notifyPayloadStarted();
 
-    // // Set the size of shared memory region
-    // constexpr auto total_memory_size = static_cast<size_t>(1 * 1024 * 1024);   // 1 MB
-    // if (ftruncate(shared_memory_field, total_memory_size) == -1)
-    // {
-    //     utils::LogString error_msg("ERROR: Failed to set the size of shared memory.\n");
-    //     std::cerr << error_msg;
-    //     close(shared_memory_field);
-    //     shm_unlink(memory_address);
-    //     return 1;
-    // }
+    puts("");
 
-    // // Map the shared memory region into the address space
-    // void* shared_memory = mmap(nullptr, total_memory_size, PROT_WRITE | PROT_READ, MAP_SHARED, shared_memory_field,
-    // 0); if (shared_memory == MAP_FAILED)
-    // {
-    //     utils::LogString error_msg("ERROR: Failed to map shared memory.\n");
-    //     std::cerr << error_msg;
-    //     close(shared_memory_field);
-    //     shm_unlink(memory_address);
-    //     return 1;
-    // }
-
-    // auto* shared_memory_counters = static_cast<SharedMemory*>(shared_memory);
+    puts("Hello world from vloop running on cpu1!");
 
     // ************************************************************
     // Create and initialize a couple of components: 3 PIDs and an RST
@@ -66,22 +38,24 @@ int main()
     component::PID pid2("pid_2", 2, 2, 2);
     component::PID pid3("pid_3", 3, 3, 3);
 
-    std::cout << std::boolalpha << (pid1.p > pid2.p) << "\n";
-
     // Freestanding parameters are not permitted, so Parameter<T> param("name", value); declarations are not allowed
     // However, the following is allowed but not a recommended approach:
     // parameters::Parameter<int> pid_1_index(pid1, "index", 1, 0, 1000);
 
     component::RST rst1("rst_1", {1.1, 2.2, 3.3, 4.4}, false);
 
-    std::cout << rst1.r[0] << "\n";   // square bracket access to underlying array
+    (std::to_string(rst1.r[0]).c_str());   // square bracket access to underlying array
 
     // No parameter declarations beyond this point!
     // ************************************************************
 
-    std::cout << "Component manifest:\n";
+    puts("Component manifest:");
     auto const& json_component_registry = component::ComponentRegistry::instance().createManifest();
-    std::cout << json_component_registry.dump(4) << "\n";
+    puts(std::to_string(json_component_registry.dump().size()).c_str());
+    bmboot::writeToStdout(
+        (json_component_registry.dump()).data(), (json_component_registry.dump().size() * sizeof(char))
+    );
+    puts("");
     // Create and initialize the shared data structure
     size_t offset
         = 2 * sizeof(size_t);   // Starting offset in the shared memory, size of two counters in the shared memory
@@ -94,45 +68,59 @@ int main()
     while (true)
     {
         utils::LogString counter_message = "Thread 1 counter: ";
-        std::cout << counter_message << counter++ << "\n";
+        puts(counter_message.c_str());
+        puts(std::to_string(counter++).c_str());
         // TEST CODE, verbose parameters signalling on thread 1
-        std::cout << "PID1: " << pid1.p << " " << pid1.i << " " << pid1.d << "\n";
-        std::cout << "PID2: " << pid2.p << " " << pid2.i << " " << pid2.d << "\n";
-        std::cout << "PID3: " << pid3.p << " " << pid3.i << " " << pid3.d << "\n";
-        std::cout << "RST1: ";
+        puts("PID1: ");
+        puts(std::to_string(pid1.p).c_str());
+        puts(std::to_string(pid1.i).c_str());
+        puts(std::to_string(pid1.d).c_str());
+        puts("PID2: ");
+        puts(std::to_string(pid2.p).c_str());
+        puts(std::to_string(pid2.i).c_str());
+        puts(std::to_string(pid2.d).c_str());
+        puts("PID3: ");
+        puts(std::to_string(pid3.p).c_str());
+        puts(std::to_string(pid3.i).c_str());
+        puts(std::to_string(pid3.d).c_str());
+        puts("RST1: ");
 
         std::for_each(
             rst1.r.cbegin(), rst1.r.cend(),
             [](auto const& val)
             {
-                std::cout << val << " ";
+                puts(std::to_string(val).c_str());
             }
         );
-        std::cout << rst1.getStatusAsStr() << " " << std::boolalpha << rst1.flag << "\n";
+        puts(std::string(rst1.getStatusAsStr()).c_str());
+        puts(std::to_string(rst1.flag).c_str());
+
         // END TEST CODE
         // TEST CODE, extremely basic execution of commands received from a remote thread, will be part of background
         // task
-        // if (shared_memory_counters->transmitted_counter > shared_memory_counters->acknowledged_counter)
-        // {
-        //     auto json_object = utils::StaticJsonFactory::getJsonObject();
-        //     json_object      = readJsonFromSharedMemory(shared_memory, offset);
-        //     offset           += json_object.dump().size();
-        //     // execute the command from the incoming stream, synchronises write and background buffers
-        //     background_task::processJsonCommands(json_object);
+        if (shared_memory_counters->transmitted_counter > shared_memory_counters->acknowledged_counter)
+        {
+            puts("Received new data!");
+            //     auto json_object = utils::StaticJsonFactory::getJsonObject();
+            //     json_object      = readJsonFromSharedMemory(shared_memory, offset);
+            //     offset           += json_object.dump().size();
+            //     // execute the command from the incoming stream, synchronises write and background buffers
+            //     background_task::processJsonCommands(json_object);
 
-        //     // acknowledge transaction
-        //     shared_memory_counters->acknowledged_counter++;
-        //     received_new_data = true;
-        // }
-        // else if (received_new_data)
-        // {
-        //     // if no new data came in the previous iteration, assume it is safe to switch the read buffers now and
-        //     // synchronise them
-        //     buffer_switch ^= 1;   // flip the buffer pointer of all settable parameters
-        //     // synchronise new background to new active buffer
-        //     background_task::synchroniseReadBuffers();
-        //     received_new_data = false;   // buffers updated, no new data available
-        // }
+            // acknowledge transaction
+            //     shared_memory_counters->acknowledged_counter++;
+            //     received_new_data = true;
+        }
+        else if (received_new_data)
+        {
+            //     // if no new data came in the previous iteration, assume it is safe to switch the read buffers now
+            //     and
+            //     // synchronise them
+            //     buffer_switch ^= 1;   // flip the buffer pointer of all settable parameters
+            //     // synchronise new background to new active buffer
+            //     background_task::synchroniseReadBuffers();
+            //     received_new_data = false;   // buffers updated, no new data available
+        }
 
         // END TEST CODE
         // Add some delay to simulate work
