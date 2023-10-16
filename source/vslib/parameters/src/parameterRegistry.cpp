043209162ff6ccd1259d20fce73f63454182f429
Dominik Arominski <dominik.arominski@cern.ch>
2023-10-16 14:30:26 +0200

    Added check ensuring parameter names are unique

--- source/vslib/parameters/src/parameterRegistry.cpp
+++ source/vslib/parameters/src/parameterRegistry.cpp
@@ -5,6 +5,8 @@
 #include <ranges>
 #include <type_traits>
 
+#include "errorMessage.h"
+#include "fmt/format.h"
 #include "parameterRegistry.h"
 
 using json = nlohmann::json;
@@ -17,6 +19,16 @@ namespace vslib::parameters
     //! @param parameter_reference Reference to the parameter being added to the parameter registry
     void ParameterRegistry::addToRegistry(std::string_view parameter_name, IParameter& parameter_reference)
     {
+        if (m_parameters.find(std::string(parameter_name)) != m_parameters.end())
+        {
+            fgc4::utils::Error error_message(
+                std::string("Parameter name: ") + std::string(parameter_name)
+                    + std::string(" already defined in the registry!\n"),
+                fgc4::utils::constants::error_name_already_used
+            );
+            std::cerr << fmt::format("{}", error_message);
+            throw std::runtime_error("Parameter name already exists!");
+        }
         m_parameters.emplace(parameter_name, parameter_reference);
     }
 }
