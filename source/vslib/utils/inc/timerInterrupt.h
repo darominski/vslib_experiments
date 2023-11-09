6186aeb0ed85a736bdfd5c5abb5dc71ca22dfd45
Dominik Arominski <dominik.arominski@cern.ch>
2023-11-09 15:04:11 +0100

    Adapted timer interrupt to bmboot v0.3

--- source/vslib/utils/inc/timerInterrupt.h
+++ source/vslib/utils/inc/timerInterrupt.h
@@ -11,18 +11,25 @@ namespace vslib
     class TimerInterrupt : public Interrupt
     {
       public:
+        //! Constructor for TimerInterrupt
+        //!
+        //! @param handler_function Function to be called when an interrupt triggers
+        //! @param microsecond_delay Delay between interrupts in integer increments of a microsecond
         TimerInterrupt(std::function<void(void)> handler_function, int microsecond_delay)
             : Interrupt(std::move(handler_function)),
               m_microsecond_delay{microsecond_delay}
         {
             assert((microsecond_delay > 0) && "Delay for the timing interrupt must be a positive number.");
+            bmboot::setupPeriodicInterrupt(m_microsecond_delay, m_interrupt_handler);
         }
 
+        //! Starts periodic interrupt
         void start() override
         {
-            bmboot::startPeriodicInterrupt(m_microsecond_delay, m_interrupt_handler);
+            bmboot::startPeriodicInterrupt();
         }
 
+        //! Stops periodic interrupt from triggering
         void stop() override
         {
             bmboot::stopPeriodicInterrupt();
