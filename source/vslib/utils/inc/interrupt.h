6587c036a9603f84d3597d383d69f42f628640bd
Dominik Arominski <dominik.arominski@cern.ch>
2023-11-15 09:49:17 +0100

    Removed merge commit error: timer interrupt is in a separate file

--- source/vslib/utils/inc/interrupt.h
+++ source/vslib/utils/inc/interrupt.h
@@ -87,28 +87,4 @@ namespace vslib
 #endif
     };
 
-    class TimerInterrupt : public Interrupt
-    {
-      public:
-        TimerInterrupt(std::function<void(void)> handler_function, int microsecond_delay)
-            : Interrupt(std::move(handler_function)),
-              m_microsecond_delay{microsecond_delay}
-        {
-            assert((microsecond_delay > 0) && "Delay for the timing interrupt must be a positive number.");
-        }
-
-        void start() override
-        {
-            bmboot::startPeriodicInterrupt(m_microsecond_delay, m_interrupt_handler);
-        }
-
-        void stop() override
-        {
-            bmboot::stopPeriodicInterrupt();
-        }
-
-      private:
-        int m_microsecond_delay;
-    };
-
 }   // namespace vslib
