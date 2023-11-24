b6d95b00b2d25aec9c5a5137e132ef539aa0746e
Dominik Arominski <dominik.arominski@cern.ch>
2023-11-24 16:53:54 +0100

    Simplified lowPassFilter to not use dynamic STL objects and operate only on array

--- source/vslib/components/inc/lowPassFilter.h
+++ source/vslib/components/inc/lowPassFilter.h
@@ -4,37 +4,35 @@
 
 #pragma once
 
-#include <deque>
+#include <array>
 #include <string>
 
 #include "component.h"
-#include "ringBufferAllocator.h"
 
 namespace vslib
 {
-
-    using DequeAllocator = fgc4::utils::StaticRingBufferAllocator<double, std::deque<double>, 1000>;
-
-    using Deque = std::deque<double, DequeAllocator>;
-
     template<size_t N>
     class LowPassFilter : public Component
     {
       public:
+        //! Constructor of the low pass filter component, initializing one Parameter: coefficients
         LowPassFilter(std::string_view name, Component* parent = nullptr)
             : Component("LowPassFilter", name, parent),
-              coefficients(*this, "coefficients"),
-              m_buffer(N, 0.0)
+              coefficients(*this, "coefficients")
         {
         }
 
+        //! Filters the provided input by convolving coefficients and the input, including previous inputs
+        //!
+        //! @param input Input value to be filtered
+        //! @return Filtered value
         double filter(double input)
         {
             shiftBuffer(input);
             double output = 0.0;
-            for (int index = 0; index < N; index++)
+            for (int64_t index = 0; index < N; index++)
             {
-                output = m_buffer[index] * coefficients[index];
+                output += coefficients[index] * m_buffer[(index + m_front - 1) % N];
             }
             return output;
         }
@@ -42,12 +40,16 @@ namespace vslib
         Parameter<std::array<double, N>> coefficients;
 
       private:
-        Deque m_buffer;
+        std::array<double, N> m_buffer{0};
+        int64_t               m_front = 0;
 
+        //! Pushes the provided value into the front of the buffer and removes the oldest value
+        //!
+        //! @param input Input value to be added to the front of the buffer
         void shiftBuffer(double input)
         {
-            m_buffer.push_front(input);
-            m_buffer.pop_back();
+            m_buffer[m_front] = input;
+            m_front           = (m_front + 1) % N;
         }
     };
 }   // namespace vslib
