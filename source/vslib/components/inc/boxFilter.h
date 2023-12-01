0a06ec011ce001a3680f0994b4d2624587f70f87
Dominik Arominski <dominik.arominski@cern.ch>
2023-12-01 18:17:44 +0100

    BoxFilter: removed separate shifting method, and corrected way to get the oldest value

--- source/vslib/components/inc/boxFilter.h
+++ source/vslib/components/inc/boxFilter.h
@@ -29,30 +29,26 @@ namespace vslib
         //! @return Filtered value
         double filter(double input) override
         {
+            int32_t oldest_value = 0;
+            if (m_filtered_counter < BufferLength)   // active only in first pass of the buffer
+            {
+                m_filtered_counter++;
+            }
+            else   // active only after the buffer has been filled
+            {
+                oldest_value = m_buffer[m_front];
+            }
             auto const input_integer = static_cast<int32_t>(m_float_to_integer * input);
-            shiftBuffer(input_integer);
-            m_cumulative += m_buffer[m_front] - m_buffer[m_front - 1];
+            m_buffer[m_front]        = input_integer;
+            m_cumulative             += input_integer - oldest_value;
+            m_front                  = (m_front + 1) % BufferLength;
             return m_integer_to_float * m_cumulative / m_filtered_counter;
         }
 
       private:
         std::array<int32_t, BufferLength> m_buffer{0};
         int32_t                           m_filtered_counter{0};
-        int32_t                           m_front{BufferLength - 1};
+        int32_t                           m_front{0};
         int32_t                           m_cumulative{0};
-
-        //! Pushes the provided value into the front of the buffer and removes the oldest value
-        //!
-        //! @param input Input value to be added to the front of the buffer
-        void shiftBuffer(int32_t input)
-        {
-            m_buffer[m_front] = input;
-            m_front--;
-            m_filtered_counter = (m_filtered_counter + 1) > BufferLength ? BufferLength : (m_filtered_counter + 1);
-            if (m_front < 0)
-            {
-                m_front = BufferLength - 1;
-            }
-        }
     };
 }   // namespace vslib
