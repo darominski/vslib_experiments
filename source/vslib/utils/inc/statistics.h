36d3dea0e4f7e7c800d0f09c8c54286e5d7e2b9f
Dominik Arominski <dominik.arominski@cern.ch>
2023-12-15 15:47:28 +0100

    Statistics: replaced template with concept

--- source/vslib/utils/inc/statistics.h
+++ source/vslib/utils/inc/statistics.h
@@ -8,24 +8,27 @@
 #include <array>
 #include <cmath>
 
+#include "typeTraits.h"
+
 namespace vslib
 {
-    template<size_t N>
-    double calculateAverage(std::array<double, N>& data)
+    double calculateAverage(auto& data)
+        requires fgc4::utils::Iterable<decltype(data)>
     {
-        return std::accumulate(data.cbegin(), data.cend(), 0.0) / N;
+        return std::accumulate(data.cbegin(), data.cend(), 0.0) / static_cast<double>(data.size());
     }
 
-    template<size_t N>
-    double calculateStandardDeviation(std::array<double, N>& data, const double mean)
+    double calculateStandardDeviation(auto& data, const double mean)
+        requires fgc4::utils::Iterable<decltype(data)>
     {
         return std::accumulate(
-            data.cbegin(), data.cend(), 0.0,
-            [&mean](const auto& value)
-            {
-                return pow((mean - value), 2) / (N - 1);
-            }
-        );
+                   data.cbegin(), data.cend(), 0.0,
+                   [&mean](const auto& lhs, const auto& value)
+                   {
+                       return lhs + pow((mean - static_cast<double>(value)), 2);
+                   }
+               )
+            / (static_cast<double>(data.size()) - 1);
     }
 
 }   // namespace vslib
