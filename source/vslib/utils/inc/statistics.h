309a61d3cdb9f5e7fbef9376a6d2fb8f09502d93
Dominik Arominski <dominik.arominski@cern.ch>
2023-12-15 17:30:29 +0100

    Statistics now in utils namespace, corrected includes

--- source/vslib/utils/inc/statistics.h
+++ source/vslib/utils/inc/statistics.h
@@ -4,13 +4,13 @@
 
 #pragma once
 
-#include <algorithm>
 #include <array>
 #include <cmath>
+#include <numeric>
 
 #include "typeTraits.h"
 
-namespace vslib
+namespace vslib::utils
 {
     double calculateAverage(auto& data)
         requires fgc4::utils::Iterable<decltype(data)>
@@ -21,14 +21,14 @@ namespace vslib
     double calculateStandardDeviation(auto& data, const double mean)
         requires fgc4::utils::Iterable<decltype(data)>
     {
-        return std::accumulate(
-                   data.cbegin(), data.cend(), 0.0,
-                   [&mean](const auto& lhs, const auto& value)
-                   {
-                       return lhs + pow((mean - static_cast<double>(value)), 2);
-                   }
-               )
-            / (static_cast<double>(data.size()) - 1);
+        double const square_sum = std::accumulate(
+            data.cbegin(), data.cend(), 0.0,
+            [&mean](const auto& lhs, const auto& value)
+            {
+                return lhs + pow((mean - static_cast<double>(value)), 2);
+            }
+        );
+        return sqrt(square_sum / (static_cast<double>(data.size()) - 1));
     }
 
 }   // namespace vslib
