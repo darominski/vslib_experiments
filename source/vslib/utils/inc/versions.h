e37f57e68ccc830a38d22f1c6a2aa1c92463de0c
Dominik Arominski <dominik.arominski@cern.ch>
2023-12-18 12:31:28 +0100

    Added version file for communication interface versioning

---
+++ source/vslib/utils/inc/versions.h
@@ -0,0 +1,13 @@
+//! @file
+//! @brief File containing versions for all used communication interfaces.
+//! @author Dominik Arominski
+
+#pragma once
+
+#include <string>
+
+namespace vslib::utils::version
+{
+    constexpr std::string_view json_command       = "0.1";
+    constexpr std::string_view json_parameter_map = "0.1";
+}
