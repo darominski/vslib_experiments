ff0791f86837266154a396f1ea817963a679cf8a
Dominik Arominski <dominik.arominski@cern.ch>
2024-02-20 15:09:59 +0100

    Limit: added change rate, integral limit window, rms time constant as parameters

--- source/vslib/components/inc/limit.h
+++ source/vslib/components/inc/limit.h
@@ -21,8 +21,11 @@ namespace vslib
               min(*this, "lower_threshold"),
               max(*this, "upper_threshold"),
               dead_zone(*this, "dead_zone"),
+              change_rate(*this, "change_rate"),
               integral_limit(*this, "integral_limit"),
-              rms(*this, "rms_threshold")
+              integral_limit_window_length(*this, "integral_limit_time_window", 0, TimeWindowLength),
+              rms(*this, "rms_threshold"),
+              rms_time_constant(*this, "rms_time_constant", 0, RMSBufferLength)
         {
         }
 
@@ -31,63 +34,98 @@ namespace vslib
         // Q3: how to efficiently check if the parameter has been set and should be used?
 
         //! Checks minimum and maximum thresholds as well as dead_zone
-        std::optional<fgc4::utils::Warning> check_min_max(T input)
+        //!
+        //! @param input Numerical input to be checked
+        //! @return Optionally returns a Warning with relevant infraction information, nothing otherwise
+        std::optional<fgc4::utils::Warning> check_min_max_limit(T input) noexcept
         {
-            if (m_is_dead_zone_defined && (input >= dead_zone[0] && input <= dead_zone[1]))
+            if (m_dead_zone_defined && (input >= dead_zone[0] && input <= dead_zone[1]))
             {
                 // ???, check which edge we are closer to and then return that?
                 return fgc4::utils::Warning(fmt::format(
-                    "Value: {} is inside the defined dead zone of [{}, {}]\n", input, dead_zone[0], dead_zone[1]
+                    "Value: {} is inside the defined dead zone of [{}, {}].\n", input, dead_zone[0], dead_zone[1]
                 ));
             }
+
             if (input <= min)
             {
                 return fgc4::utils::Warning(
-                    fmt::format("Value: {} is below or equal to the minimal value of {}\n", input, min)
+                    fmt::format("Value: {} is below or equal to the minimal value of {}.\n", input, min)
                 );
             }
             if (input >= max)
             {
                 return fgc4::utils::Warning(
-                    fmt::format("Value: {} is above of equal to the maximal value of {}\n", input, max)
+                    fmt::format("Value: {} is above of equal to the maximal value of {}.\n", input, max)
                 );
             }
 
             return {};
         }
 
+        //! Checks the rate of change of the input
+        //!
+        //! @param input Numerical input to be checked
+        //! @return Optionally returns a Warning with relevant infraction information, nothing otherwise
+        std::optional<fgc4::utils::Warning> check_change_rate_limit(T input)
+        {
+            if (input - m_previous_value > change_rate)
+            {
+                auto const& warning_msg = fgc4::utils::Warning(fmt::format(
+                    "Value: {} with difference of {} is above the maximal rate of change of: {}.\n", input,
+                    input - m_previous_value, change_rate
+                ));
+                m_previous_value        = input;
+                return warning_msg;
+            }
+            m_previous_value = input;
+            return {};
+        }
+
         //! Checks cumulative (integral) limits
         //!
         //! @param input Numerical input to be checked
-        std::optional<fgc4::utils::Warning> check_integral_limit(T input)
+        //! @return Optionally returns a Warning with relevant infraction information, nothing otherwise
+        std::optional<fgc4::utils::Warning> check_integral_limit(T input) noexcept
         {
             integral_buffer[m_head_integral] = input;
-            if (std::accumulate(std::cbegin(integral_buffer), std::cend(integral_buffer), 0) >= integral_limit)
+            m_head_integral++;
+            if (m_head_integral >= integral_limit_window_length)
             {
-                return fgc4::utils::Warning(
-                    fmt::format("Value: {} leads to overflow of the integral limit of {}\n", input, integral_limit)
-                );
+                m_head_integral -= integral_limit_window_length;
             }
 
-            m_head_integral++;
-            if (m_head_integral >= TimeWindowLength)
+            if (std::accumulate(
+                    std::cbegin(integral_buffer), std::cbegin(integral_buffer) + integral_limit_window_length, 0
+                )
+                >= integral_limit)
             {
-                m_head_integral -= TimeWindowLength;
+                return fgc4::utils::Warning(
+                    fmt::format("Value: {} leads to overflow of the integral limit of {}.\n", input, integral_limit)
+                );
             }
+            return {};
         }
 
         //! Checks the RMS limits
         //!
         //! @param input Numerical input to be checked against set RMS limit
-        std::optional<fgc4::utils::Warning> check_rms_limit(T input)
+        //! @return Optionally returns a Warning with relevant infraction information, nothing otherwise
+        std::optional<fgc4::utils::Warning> check_rms_limit(T input) noexcept
         {
             rms_buffer[m_head_rms] = input;
+            m_head_rms++;
+            if (m_head_rms >= rms_time_constant)
+            {
+                m_head_rms -= rms_time_constant;
+            }
+
             if (sqrt(
                     std::accumulate(
-                        std::cbegin(rms_buffer), std::cend(rms_buffer), 0.0,
-                        [](const auto& element)
+                        std::begin(rms_buffer), std::begin(rms_buffer) + rms_time_constant, 0.0,
+                        [](auto lhs, auto const& element)
                         {
-                            return pow(element, 2);
+                            return lhs + pow(element, 2);
                         }
                     )
                     / RMSBufferLength
@@ -95,63 +133,88 @@ namespace vslib
                 >= rms)
             {
                 return fgc4::utils::Warning(
-                    fmt::format("Value: {} deviates too far from the RMS limit of {}\n", input, rms)
+                    fmt::format("Value: {} deviates too far from the RMS limit of {}.\n", input, rms)
                 );
-            }
-            m_head_rms++;
-            if (m_head_rms >= RMSBufferLength)
-            {
-                m_head_rms -= RMSBufferLength;
             }
 
             return {};
         }
 
-
-        std::optional<fgc4::utils::Warning> limit(T input)
+        //! Checks all the specified limits
+        //!
+        //! @param input Value to be checked against all specified limits
+        //! @return Optionally returns a Warning with relevant infraction information, nothing otherwise
+        std::optional<fgc4::utils::Warning> limit(T input) noexcept
             requires fgc4::utils::Numeric<decltype(input)>
         {
             // checking straight min/max thresholds with a possible dead-zone
-            auto& maybe_warning = check_min_max(input);
-            if (maybe_warning.has_value())
+            const auto& maybe_warning_min_max = check_min_max_limit(input);
+            if (maybe_warning_min_max.has_value())
             {
-                return maybe_warning.value();
+                return maybe_warning_min_max.value();
             }
 
-            maybe_warning = check_integral_limit(input);
-            if (maybe_warning.has_value())
+            const auto& maybe_warning_change_rate = check_change_rate_limit(input);
+            if (maybe_warning_change_rate.has_value())
             {
-                return maybe_warning.value();
+                return maybe_warning_change_rate.value();
             }
 
-            maybe_warning = check_rms_limit(input);
-            if (maybe_warning.has_value())
+            const auto& maybe_warning_integral = check_integral_limit(input);
+            if (maybe_warning_integral.has_value())
             {
-                return maybe_warning.value();
+                return maybe_warning_integral.value();
             }
+
+            const auto& maybe_warning_rms = check_rms_limit(input);
+            if (maybe_warning_rms.has_value())
+            {
+                return maybe_warning_rms.value();
+            }
+
             return {};
+        }
+
+        //! Resets the component to the initial state of buffers and buffer pointers
+        void reset() noexcept
+        {
+            m_head_integral = 0;
+            m_head_rms      = 0;
+            std::fill(std::begin(integral_buffer), std::end(integral_buffer), 0);
+            std::fill(std::begin(rms_buffer), std::end(rms_buffer), 0);
         }
 
         Parameter<T>                min;
         Parameter<T>                max;
         Parameter<std::array<T, 2>> dead_zone;
+        Parameter<T>                change_rate;
         Parameter<T>                integral_limit;
+        Parameter<size_t>           integral_limit_window_length;
         Parameter<double>           rms;
+        Parameter<size_t>           rms_time_constant;
 
+        //! Verifies parameters after any Parameter has been modified
+        //!
+        //! @return Optionally returns a warning if any issues have been found
         std::optional<fgc4::utils::Warning> verifyParameters() override
         {
-            if (dead_zone.isInitialized() && (dead_zone[0] < dead_zone[1]))
+            if (dead_zone.isInitialized() && (dead_zone[0] > dead_zone[1]))
             {
                 return fgc4::utils::Warning("Upper edge of the dead_zone is below the lower edge.\n");
             }
 
             if (dead_zone.isInitialized() && (dead_zone[0] != dead_zone[1]))
             {
-                m_is_dead_zone_defined = true;
+                m_dead_zone_defined = true;
             }
             else
             {
-                m_is_dead_zone_defined = false;
+                m_dead_zone_defined = false;
+            }
+
+            if ((min.isInitialized() && max.isInitialized()) && (min >= max))
+            {
+                return fgc4::utils::Warning("Attempted to set the lower limit below the upper limit.\n");
             }
 
             return {};
@@ -161,9 +224,11 @@ namespace vslib
         int64_t m_head_integral{0};
         int64_t m_head_rms{0};
 
+        T m_previous_value{};
+
         std::array<T, TimeWindowLength> integral_buffer{0};
         std::array<T, RMSBufferLength>  rms_buffer{0};
 
-        bool m_is_dead_zone_defined{false};
+        bool m_dead_zone_defined{false};
     };
 }
