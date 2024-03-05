7995779c35ba277eac165c65fe6e85964fd71527
Dominik Arominski <dominik.arominski@cern.ch>
2024-03-05 16:38:05 +0100

    LimitRms: optimised RMS calculation with caching cumulative

--- source/vslib/components/inc/limitRms.h
+++ source/vslib/components/inc/limitRms.h
@@ -12,13 +12,13 @@
 
 namespace vslib
 {
-    template<typename T, size_t RMSBufferLength = 16>
-    class Limit : public Component
+    template<size_t RMSBufferLength = 16>
+    class LimitRms : public Component
     {
       public:
-        Limit(std::string_view name, Component* parent = nullptr)
-            : Component("Limit", name, parent),
-              rms(*this, "rms_threshold"),
+        LimitRms(std::string_view name, Component* parent = nullptr)
+            : Component("LimitRms", name, parent),
+              rms_limit(*this, "rms_threshold"),
               rms_time_constant(*this, "rms_time_constant", 0, RMSBufferLength)
         {
         }
@@ -27,29 +27,22 @@ namespace vslib
         //!
         //! @param input Numerical input to be checked against set RMS limit
         //! @return Optionally returns a Warning with relevant infraction information, nothing otherwise
-        std::optional<fgc4::utils::Warning> limit(T input) noexcept
+        std::optional<fgc4::utils::Warning> limit(double input) noexcept
         {
-            rms_buffer[m_head_rms] = input;
-            m_head_rms++;
-            if (m_head_rms >= rms_time_constant)
+            double const new_entry = abs(input) / sqrt(RMSBufferLength);
+            m_cumulative           += new_entry - rms_buffer[m_head];
+
+            rms_buffer[m_head] = new_entry;
+            m_head++;
+            if (m_head >= rms_time_constant)
             {
-                m_head_rms -= rms_time_constant;
+                m_head -= rms_time_constant;
             }
 
-            if (sqrt(
-                    std::accumulate(
-                        std::begin(rms_buffer), std::begin(rms_buffer) + rms_time_constant, 0.0,
-                        [](auto lhs, auto const& element)
-                        {
-                            return lhs + pow(element, 2);
-                        }
-                    )
-                    / RMSBufferLength
-                )
-                >= rms)
+            if (m_cumulative > rms_limit)
             {
                 return fgc4::utils::Warning(
-                    fmt::format("Value: {} deviates too far from the RMS limit of {}.\n", input, rms)
+                    fmt::format("Value: {} deviates too far from the RMS limit of {}.\n", input, rms_limit)
                 );
             }
 
@@ -59,16 +52,18 @@ namespace vslib
         //! Resets the component to the initial state of buffers and buffer pointers
         void reset() noexcept
         {
-            m_head_rms = 0;
+            m_head = 0;
             std::fill(std::begin(rms_buffer), std::end(rms_buffer), 0);
         }
 
-        Parameter<double> rms;
+        Parameter<double> rms_limit;
         Parameter<size_t> rms_time_constant;
 
       private:
-        int64_t m_head_rms{0};
+        int64_t m_head{0};
 
-        std::array<T, RMSBufferLength> rms_buffer{0};
+        double m_cumulative{0};
+
+        std::array<double, RMSBufferLength> rms_buffer{0};
     };
 }   // namespace vslib
