50743f74fe131f7480c34cc7777c95530d6b1c06
Dominik Arominski <dominik.arominski@cern.ch>
2024-03-05 15:24:19 +0100

    LimitIntegral: optimised integral calculation by caching the cumulative

--- source/vslib/components/inc/limitIntegral.h
+++ source/vslib/components/inc/limitIntegral.h
@@ -29,38 +29,40 @@ namespace vslib
         //! @return Optionally returns a Warning with relevant infraction information, nothing otherwise
         std::optional<fgc4::utils::Warning> limit(T input) noexcept
         {
-            integral_buffer[m_head_integral] = input;
-            m_head_integral++;
-            if (m_head_integral >= integral_limit_window_length)
+            m_cumulative += (input - m_integral_buffer[m_head]);
+
+            m_integral_buffer[m_head] = input;
+            m_head++;
+            if (m_head >= integral_limit_window_length)
             {
-                m_head_integral -= integral_limit_window_length;
+                m_head -= integral_limit_window_length;
             }
 
-            if (std::accumulate(
-                    std::cbegin(integral_buffer), std::cbegin(integral_buffer) + integral_limit_window_length, 0
-                )
-                >= integral_limit)
+            if (m_cumulative >= integral_limit)
             {
                 return fgc4::utils::Warning(
                     fmt::format("Value: {} leads to overflow of the integral limit of {}.\n", input, integral_limit)
                 );
             }
+
             return {};
         }
 
         //! Resets the component to the initial state of buffers and buffer pointers
         void reset() noexcept
         {
-            m_head_integral = 0;
-            std::fill(std::begin(integral_buffer), std::end(integral_buffer), 0);
+            m_head = 0;
+            std::fill(std::begin(m_integral_buffer), std::end(m_integral_buffer), 0);
         }
 
         Parameter<T>      integral_limit;
         Parameter<size_t> integral_limit_window_length;
 
       private:
-        int64_t m_head_integral{0};
+        int64_t m_head{0};
 
-        std::array<T, TimeWindowLength> integral_buffer{0};
+        T m_cumulative{T{}};
+
+        std::array<T, TimeWindowLength> m_integral_buffer{0};
     };
 }   // namespace vslib
