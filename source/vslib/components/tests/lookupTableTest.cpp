58cc290d26398ad3d3d317702d578fd558b5d571
Dominik Arominski <dominik.arominski@cern.ch>
2024-03-22 14:54:53 +0100

    Lookup table: tests for consistency of the binary search vs linear search behaviour

--- source/vslib/components/tests/lookupTableTest.cpp
+++ source/vslib/components/tests/lookupTableTest.cpp
@@ -133,14 +133,35 @@ TEST_F(LookupTableTest, LookupTableDoubleNegativeAxis)
     EXPECT_NEAR(table.interpolate(-3.0), 3.3, 1e-15);
 }
 
+//! Tests LookupTable component with a meaningful double table and then interpolating with a more realistic case of
+//! interpolation input being somewhere between the data points
+TEST_F(LookupTableTest, LookupTableDoubleInterpolateBetweenPoints)
+{
+    std::string                            name = "table";
+    std::vector<std::pair<double, double>> values{{-3.0, 3.3}, {-2.0, 2.3}, {-1.0, 1.3}, {0.0, 0.3}};
+    LookupTable<double>                    table(name, nullptr, std::move(values));
+
+    EXPECT_NEAR(table.interpolate(-2.5), 0.5 * (3.3 + 2.3), 1e-15);
+    EXPECT_NEAR(table.interpolate(-1.5), 0.5 * (2.3 + 1.3), 1e-15);
+    EXPECT_NEAR(table.interpolate(-0.5), 0.5 * (1.3 + 0.3), 1e-15);
+
+    // and check that nothing goes wrong if we do the same in reverse order:
+    EXPECT_NEAR(table.interpolate(-0.5), 0.5 * (1.3 + 0.3), 1e-15);
+    EXPECT_NEAR(table.interpolate(-1.5), 0.5 * (2.3 + 1.3), 1e-15);
+    EXPECT_NEAR(table.interpolate(-2.5), 0.5 * (3.3 + 2.3), 1e-15);
+}
+
+
 //! Tests LookupTable provides the expected saturation behaviour the input is below the provided data limits
-TEST_F(LookupTableTest, LookupTableIntInterpolateBelowLimits)
+TEST_F(LookupTableTest, LookupTableIntInterpolateBelowLimitsConsistency)
 {
     std::string                      name = "table";
     std::vector<std::pair<int, int>> data{{-3, 3}, {-2, 2}, {-1, 1}, {0, 0}};
     LookupTable<int>                 table(name, nullptr, std::move(data));
 
     EXPECT_EQ(table.interpolate(-4), 3);
+    EXPECT_EQ(table.interpolate(-100), 3);
+    EXPECT_EQ(table.interpolate(-15), table.interpolate(-1000));
 }
 
 //! Tests LookupTable provides the expected saturation behaviour the input is above the provided data limits
@@ -151,4 +172,38 @@ TEST_F(LookupTableTest, LookupTableIntInterpolateAboveLimits)
     LookupTable<int>                 table(name, nullptr, std::move(data));
 
     EXPECT_EQ(table.interpolate(4), 0);
+}
+
+//! Tests LookupTable provides the expected output regardless whether the switch for random access is true or not
+TEST_F(LookupTableTest, LookupTableIntRandomAccessConsistency)
+{
+    std::string                         name = "table";
+    std::vector<std::pair<double, int>> data{{-3, 3}, {-2, 2}, {-1, 1}, {0, 0}};
+    LookupTable<double, int>            table(name, nullptr, std::move(data));
+
+    EXPECT_EQ(table.interpolate(-3.5), table.interpolate(-3.5, true));
+    EXPECT_EQ(table.interpolate(-3), table.interpolate(-3, true));
+    EXPECT_EQ(table.interpolate(-2.5), table.interpolate(-2.5, true));
+    EXPECT_EQ(table.interpolate(-2), table.interpolate(-2, true));
+    EXPECT_EQ(table.interpolate(-1.5), table.interpolate(-1.5, true));
+    EXPECT_EQ(table.interpolate(-1), table.interpolate(-1, true));
+    EXPECT_EQ(table.interpolate(-0.5), table.interpolate(-0.5, true));
+    EXPECT_EQ(table.interpolate(0), table.interpolate(0, true));
+}
+
+//! Tests LookupTable provides the expected output regardless whether the switch for random access is true or not
+TEST_F(LookupTableTest, LookupTableDoubleRandomAccessConsistency)
+{
+    std::string                            name = "table";
+    std::vector<std::pair<double, double>> data{{-3, 3}, {-2, 2}, {-1, 1}, {0, 0}};
+    LookupTable<double>                    table(name, nullptr, std::move(data));
+
+    EXPECT_EQ(table.interpolate(-3.5), table.interpolate(-3.5, true));
+    EXPECT_EQ(table.interpolate(-3), table.interpolate(-3, true));
+    EXPECT_EQ(table.interpolate(-2.5), table.interpolate(-2.5, true));
+    EXPECT_EQ(table.interpolate(-2), table.interpolate(-2, true));
+    EXPECT_EQ(table.interpolate(-1.5), table.interpolate(-1.5, true));
+    EXPECT_EQ(table.interpolate(-1), table.interpolate(-1, true));
+    EXPECT_EQ(table.interpolate(-0.5), table.interpolate(-0.5, true));
+    EXPECT_EQ(table.interpolate(0), table.interpolate(0, true));
 }
