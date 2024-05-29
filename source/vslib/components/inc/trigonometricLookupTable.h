e56cded26f48937122d1338179762e4df539eaba
Dominik Arominski <dominik.arominski@cern.ch>
2024-05-29 16:18:36 +0200

    Renamed trigonometryLookupTable to trigonometricLookupTable

--- source/vslib/components/inc/trigonometricLookupTable.h
+++ source/vslib/components/inc/trigonometricLookupTable.h
@@ -13,31 +13,32 @@
 
 namespace vslib
 {
-    enum class TrigonometryFunctions
+    enum class TrigonometricFunctions
     {
         sin,
         cos
     };
 
-    class TrigonometryLookupTable : public Component
+    class TrigonometricLookupTable : public Component
     {
       public:
-        TrigonometryLookupTable(
-            std::string_view name, Component* parent, TrigonometryFunctions choice, size_t number_points
+        TrigonometricLookupTable(
+            std::string_view name, Component* parent, TrigonometricFunctions choice, size_t number_points
         )
-            : Component("TrigonometryLookupTable", name, parent),
+            : Component("TrigonometricLookupTable", name, parent),
               m_function("data", this)
         {
+            assert(number_points >= 2);
             std::vector<std::pair<double, double>> data(number_points);
             for (size_t index = 0; index < number_points; index++)
             {
                 const double x = (2.0 * M_PI * index) / number_points;   // 0 - 2pi
                 double       y;
-                if (choice == TrigonometryFunctions::sin)
+                if (choice == TrigonometricFunctions::sin)
                 {
                     y = sin(x);
                 }
-                else if (choice == TrigonometryFunctions::cos)
+                else if (choice == TrigonometricFunctions::cos)
                 {
                     y = cos(x);
                 }
@@ -52,7 +53,6 @@ namespace vslib
             return m_function.interpolate(input_x);
         }
 
-      private:
         PeriodicLookupTable<double, double> m_function;
     };
 }
