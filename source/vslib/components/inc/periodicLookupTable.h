07612a681472b5615cdb65c0a500f89c81e16d6a
Dominik Arominski <dominik.arominski@cern.ch>
2024-05-31 15:07:30 +0200

    LookupTable: extracted interpolation logic to own helper method

--- source/vslib/components/inc/periodicLookupTable.h
+++ source/vslib/components/inc/periodicLookupTable.h
@@ -52,47 +52,8 @@ namespace vslib
                 }
                 input_x += this->m_lower_edge_x;
             }
-            size_t     start_loop_index = 0;
-            IndexType  x1, x2;
-            StoredType y1, y2;
 
-            if (input_x >= this->m_previous_section_x[0])
-            {
-                if (input_x < this->m_previous_section_x[1])   // same section
-                {
-                    return this->m_previous_section_y
-                           + (input_x - this->m_previous_section_x[1]) * this->m_interpolation_factor;
-                }   // else: new section and we need to find new edges
-                start_loop_index = this->m_previous_section_index;
-            }
-
-            if (this->m_equal_binning)
-            {
-                // Going branchless with constexpr if and placing m_equal_binning in the template does not provide any
-                // benefit in this case
-
-                // This case provides a 15% speedup for a 100-element lookup table when compared with linear
-                // time monotonic access from the 'else' case.
-
-                utils::index_search(this->m_values, input_x, this->m_lower_edge_x, this->m_bin_size, x1, y1, x2, y2);
-            }
-            else
-            {
-                // Existence of this branch leads to a loss of 1% performance.
-
-                // binary_search performs a binary search, more efficient with random access, while
-                // for monotonic access the linear linear_search should be more efficient assuming that the next
-                // point is relatively close to the previously interpolated one.
-                this->m_previous_section_index
-                    = random_access ? utils::binary_search(this->m_values, input_x, start_loop_index, x1, y1, x2, y2)
-                                    : utils::linear_search(this->m_values, input_x, start_loop_index, x1, y1, x2, y2);
-            }
-            this->m_previous_section_y    = y1;
-            this->m_previous_section_x[1] = x1;
-            this->m_previous_section_x[0] = x2;
-            this->m_interpolation_factor  = (y2 - y1) / (x2 - x1);
-
-            return y1 + (input_x - x1) * this->m_interpolation_factor;
+            return this->interpolate_data(input_x, random_access);
         }
 
       private:
