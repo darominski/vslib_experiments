74aec9659ad3f2edffc6724674e0d4039b0fa8aa
Dominik Arominski <dominik.arominski@cern.ch>
2024-06-07 17:14:41 +0200

    BoxFilter: move the specializations to source file

--- source/vslib/components/inc/boxFilter.h
+++ source/vslib/components/inc/boxFilter.h
@@ -57,7 +57,7 @@ namespace vslib
         //! Returns the maximum value that can be filtered by this filter
         //!
         //! @return Maximal value that can be filter by this filter
-        [[nodiscard]] static auto const getMaxInputValue()
+        [[nodiscard]] static auto constexpr getMaxInputValue()
         {
             return FixedPoint<fractional_bits>::maximum_value;
         }
@@ -66,86 +66,5 @@ namespace vslib
         std::array<FixedPoint<fractional_bits>, buffer_length> m_buffer{0};
         uint64_t                                               m_head{0};
         FixedPoint<fractional_bits>                            m_cumulative{0};
-    };
-
-    // ************************************************************
-    // Partial template specialization for low-order filters
-    //
-    // Benchmarking showed 126% gain for the first order, and 50% for the 2nd order.
-
-    template<>
-    class BoxFilter<1> : public Filter
-    {
-
-      public:
-        //! Constructor of the box filter component
-        BoxFilter(std::string_view name, Component* parent)
-            : Filter("BoxFilter", name, parent)
-        {
-        }
-
-        //! Filters the provided input by calculating the moving average of the buffer of previously
-        //! provided inputs
-        //!
-        //! @param input Input value to be filtered
-        //! @return Filtered value
-        [[nodiscard]] double filter(double input) override
-        {
-            double const result = input + m_previous_value;
-
-            m_previous_value = input;
-
-            return result / 2.0;
-        }
-
-        //! Returns the maximum value that can be filtered by this filter
-        //!
-        //! @return Maximal value that can be filter by this filter
-        [[nodiscard]] static auto const getMaxInputValue()
-        {
-            return std::numeric_limits<double>::max();
-        }
-
-      private:
-        double m_previous_value{0};   // input value one iteration earlier
-    };
-
-    template<>
-    class BoxFilter<2> : public Filter
-    {
-
-      public:
-        //! Constructor of the box filter component
-        BoxFilter(std::string_view name, Component* parent)
-            : Filter("BoxFilter", name, parent)
-        {
-        }
-
-        //! Filters the provided input by calculating the moving average of the buffer of previously
-        //! provided inputs
-        //!
-        //! @param input Input value to be filtered
-        //! @return Filtered value
-        [[nodiscard]] double filter(double input) override
-        {
-            double const result = input + m_previous_value + m_earlier_value;
-
-            m_earlier_value  = m_previous_value;
-            m_previous_value = input;
-
-            return result / 3.0;
-        }
-
-        //! Returns the maximum value that can be filtered by this filter
-        //!
-        //! @return Maximal value that can be filter by this filter
-        [[nodiscard]] static auto const getMaxInputValue()
-        {
-            return std::numeric_limits<double>::max();
-        }
-
-      private:
-        double m_previous_value{0};   // input value one iteration earlier
-        double m_earlier_value{0};    // input value two iterations earlier
     };
 }   // namespace vslib
