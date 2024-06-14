1be56e201ddbb527eb31f4bf5df72dcd926acc99
Dominik Arominski <dominik.arominski@cern.ch>
2024-06-14 09:26:50 +0200

    Moved filter overrides to the same translation unit

--- source/vslib/components/inc/firFilter.h
+++ source/vslib/components/inc/firFilter.h
@@ -100,4 +100,45 @@ namespace vslib
         }
     };
 
+    // ************************************************************
+    // Overrides for low-order filters. Specialization of functions avoids repetition
+    // of the entire class structure.
+    //
+    // Benchmarking showed 44% gain for the first order, and 72% for the 2nd order.
+
+    //! Filters the provided input using 1st order specialization by convolving coefficients and the input, including
+    //! previous input
+    //!
+    //! @param input Input value to be filtered
+    //! @return Filtered value
+    template<>
+    [[nodiscard]] inline double FIRFilter<1>::filter(const double input)
+    {
+        auto const   previous_input = m_buffer[0];
+        double const output         = input * m_coefficients[0] + previous_input * m_coefficients[1];
+        m_buffer[0]                 = input;   // update input buffer
+
+        return output;
+    }
+
+    //! Filters the provided input using 2nd order specialization by convolving coefficients and the input, including
+    //! previous inputs
+    //!
+    //! @param input Input value to be filtered
+    //! @return Filtered value
+    template<>
+    [[nodiscard]] inline double FIRFilter<2>::filter(const double input)
+    {
+        auto const earlier_input  = m_buffer[0];
+        auto const previous_input = m_buffer[1];
+
+        double const output
+            = input * m_coefficients[0] + previous_input * m_coefficients[1] + earlier_input * m_coefficients[2];
+
+        // update input buffer
+        m_buffer[0] = m_buffer[1];
+        m_buffer[1] = input;
+        return output;
+    }
+
 }   // namespace vslib
