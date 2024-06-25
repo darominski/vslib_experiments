e7da3e740edc23d93b6a54e0e2c063681cca7ee0
Dominik Arominski <dominik.arominski@cern.ch>
2024-06-25 12:18:13 +0200

    ClarkeParkTransform: simplified transform with alignment options

--- source/vslib/components/src/clarkeParkTransform.cpp
+++ source/vslib/components/src/clarkeParkTransform.cpp
@@ -19,34 +19,12 @@ namespace vslib
         const double sin_theta = m_sin(theta);
         const double cos_theta = m_cos(theta);
 
-        double d;
-        double q;
-        double zero;
+        const double d
+            = a_alignment ? f_alpha * cos_theta + f_beta * sin_theta : f_alpha * sin_theta - f_beta * cos_theta;
+        const double q
+            = a_alignment ? -f_alpha * sin_theta + f_beta * cos_theta : f_alpha * cos_theta + f_beta * sin_theta;
 
-        if (a_alignment)   // A-axis alignment
-        {
-            d    = f_alpha * cos_theta + f_beta * sin_theta;
-            q    = -f_alpha * sin_theta + f_beta * cos_theta;
-            zero = f_0;
-        }
-        else   // 90 degrees behind A axis
-        {
-            const double u_a = f_alpha + f_0;
-            const double u_b = 0.5 * (-f_alpha + std::numbers::sqrt3 * f_beta) + f_0;
-            const double u_c = -0.5 * (f_alpha + std::numbers::sqrt3 * f_beta) + f_0;
-
-            const double sin_theta_minus_two_thirds = m_sin(theta - two_pi_over_3);
-            const double sin_theta_plus_two_thirds  = m_sin(theta + two_pi_over_3);
-
-            const double cos_theta_minus_two_thirds = m_cos(theta - two_pi_over_3);
-            const double cos_theta_plus_two_thirds  = m_cos(theta + two_pi_over_3);
-
-            d    = two_over_3 * (u_a * sin_theta + u_b * sin_theta_minus_two_thirds + u_c * sin_theta_plus_two_thirds);
-            q    = two_over_3 * (u_a * cos_theta + u_b * cos_theta_minus_two_thirds + u_c * cos_theta_plus_two_thirds);
-            zero = one_over_3 * (u_a + u_b + u_c);
-        }
-
-        return {d, q, zero};
+        return {d, q, f_0};
     }
 
 }   // namespace vslib
