6bc358e2d09de57209c530965dc096294a5d0208
Dominik Arominski <dominik.arominski@cern.ch>
2024-08-09 16:06:24 +0200

    Docs: improved conversion from eps to png by using find -exec

--- source/vslib/docs/source/figures/convert_all_to_png.sh
+++ source/vslib/docs/source/figures/convert_all_to_png.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-ls *eps | { eps_name=$(< /dev/stdin); gs -dSAFER -dEPSCrop -r600 -sDEVICE=pngalpha -o ${eps_name%.eps*}.png ${eps_name}; }
-ls *drawio | { drawio_name=$(< /dev/stdin); drawio -x -f png -s 1 ${drawio_name}; }
+find . -name '*.eps' -exec sh -c 'gs -dSAFER -dEPSCrop -r600 -sDEVICE=pngalpha -o "${0%.eps}.png" "$0"' {} \;
