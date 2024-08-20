83904d02bbebb609dce2ec10f5d125477e87a64c
Dominik Arominski <dominik.arominski@cern.ch>
2024-08-20 14:40:06 +0200

    Linux app: updated to run with Kria K26

--- source/linux_parameter_setter/remote.cpp
+++ source/linux_parameter_setter/remote.cpp
@@ -1,5 +1,7 @@
+#include <array>
 #include <fcntl.h>
 #include <iostream>
+#include <map>
 #include <string>
 #include <unistd.h>
 
@@ -15,22 +17,46 @@ using namespace vslib;
 using namespace fgcd;
 using namespace fgc4;
 
+void parseComponent(
+    const Json& component, const std::string& base_name,
+    std::vector<std::pair<std::string, std::string>>& settable_parameters
+)
+{
+    if (!component.contains("parameters"))
+    {
+        return;
+    }
+    const auto& parameters          = component["parameters"];   // array of settable parameters;
+    const auto  full_component_name = base_name + "." + std::string(component["name"]);
+    for (const auto& parameter : parameters)
+    {
+        settable_parameters.emplace_back(
+            std::make_pair(full_component_name + "." + std::string(parameter["name"]), parameter["type"])
+        );
+    }
+
+    // recursively traverse hierarchy:
+    if (component.contains("components"))
+    {
+        for (const auto& child : component["components"])
+        {
+            parseComponent(child, full_component_name, settable_parameters);
+        }
+    }
+}
+
 auto parseManifest(const Json& manifest)
 {
     std::vector<std::pair<std::string, std::string>> settable_parameters;
     for (auto const& element : manifest)
     {
-        if (!element.contains("parameters"))   // version of the parameter map
+        if (!element.contains("components"))
         {
             continue;
         }
-        auto const& parameters        = element["parameters"];   // array of settable parameters;
-        auto const& full_component_id = std::string(element["type"]) + "." + std::string(element["name"]);
-        for (auto const& parameter : parameters)
+        for (const auto& component : element["components"])
         {
-            settable_parameters.emplace_back(
-                std::make_pair(full_component_id + "." + std::string(parameter["name"]), parameter["type"])
-            );
+            parseComponent(component, element["name"], settable_parameters);
         }
     }
     return settable_parameters;
@@ -38,59 +64,36 @@ auto parseManifest(const Json& manifest)
 
 auto prepareCommands(const std::vector<std::pair<std::string, std::string>>& parameters)
 {
-    std::vector<Json>  commands;
-    std::array<int, 2> version{0, 1};
-    for (const auto& [name, type] : parameters)
+    std::vector<Json>           commands;
+    std::array<int, 2>          version{0, 1};
+    std::map<std::string, Json> values_to_set;
+    values_to_set["kp"]                      = 1.0;
+    values_to_set["kd"]                      = 1.1;
+    values_to_set["ki"]                      = 1.2;
+    values_to_set["kff"]                     = 1.3;
+    values_to_set["proportional_scaling"]    = 1.4;
+    values_to_set["derivative_scaling"]      = 1.5;
+    values_to_set["derivative_filter_order"] = 1.6;
+    values_to_set["control_period"]          = 1.7;
+    values_to_set["pre-warping_frequency"]   = 1.8;
+    values_to_set["lower_threshold"]         = -1.0e9;
+    values_to_set["upper_threshold"]         = 1.0e9;
+    values_to_set["dead_zone"]               = std::array<double, 2>{0, 0};
+
+    for (const auto& [name, _] : parameters)
     {
-        Json command = {{"name", name}};
-        command.push_back({"version", version});
-        if (type == "Float64")
-        {
-            // half of the values will be invalid
-            // if (commands.size() % 2 == 0)
-            // {
-            double value = 3.14159 * static_cast<double>(commands.size() % 2);
-            command.push_back({"value", value});
-            // }
-            // else
-            // {
-            //     std::string value = "invalid";
-            //     command.push_back({"value", value});
-            // }
-        }
-        else if (type == "Bool")
-        {
-            command.push_back({"value", true});
-        }
-        else if (type == "Enum")
-        {
-            command.push_back({"value", "updating"});
-        }
-        else if (type == "ArrayFloat64")
-        {
-            std::array<double, 3> values = {1.0, 1.1, 1.3};
-            command.push_back({"value", values});
-        }
-        else
-        {
-            int value = -123;
-            command.push_back({"value", value});
-            // int const             counter = static_cast<int>(commands.size());
-            // std::array<double, 4> value   = {counter + 0.1, counter + 1.1, counter + 2.2, counter + 3.3};
-            // command.push_back({"value", value});
-        }
-        commands.push_back(command);
+        std::string param_name = name.substr(name.find_last_of(".") + 1, name.size());
+        Json        command    = {{"name", name}, {"version", version}, {"value", values_to_set[param_name]}};
+        commands.emplace_back(command);
     }
-    commands.push_back({"misformed", "command"});
-    commands.push_back({{"name", "ComponentArray.brick_2.PID.brick_2_1.p"}, {"value", 0.2718}});
     return commands;
 }
 
-int main()
+int main(int argc, char* argv[])
 {
     fprintf(stderr, "Start Bmboot\n");
 
-    auto domain = bmboot::throwOnError(bmboot::IDomain::open(bmboot::DomainIndex::cpu1), "IDomain::open");
+    auto domain = bmboot::throwOnError(bmboot::IDomain::open(bmboot::DomainIndex::cpu3), "IDomain::open");
     bmboot::throwOnError(domain->ensureReadyToLoadPayload(), "ensureReadyToLoadPayload");
 
     fprintf(stderr, "Map memory\n");
@@ -98,12 +101,12 @@ int main()
     constexpr int string_queue_size = fgc4::utils::constants::string_memory_pool_size;
 
     int          fd = open("/dev/mem", O_RDWR);
-    bmboot::Mmap buffer(nullptr, 64 * json_queue_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, app_data_0_1_ADDRESS);
+    bmboot::Mmap buffer(nullptr, 64 * json_queue_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, app_data_2_3_ADDRESS);
 
     fprintf(stderr, "Init message queues\n");
     // one read queue for reading the parameter map and one queue for writing commands
 
-    constexpr size_t read_commands_queue_address = app_data_0_1_ADDRESS;
+    constexpr size_t read_commands_queue_address = app_data_2_3_ADDRESS;
     constexpr size_t write_commands_status_queue_address
         = read_commands_queue_address + fgc4::utils::constants::json_memory_pool_size;
     constexpr size_t write_parameter_map_queue_address = read_commands_queue_address
@@ -123,7 +126,7 @@ int main()
     );
 
     fprintf(stderr, "Run payload\n");
-    bmboot::loadPayloadFromFileOrThrow(*domain, "vloop_cpu1.bin");
+    bmboot::loadPayloadFromFileOrThrow(*domain, argv[1]);
     usleep(500'000);   // delay for initialization
 
     std::array<uint8_t, json_queue_size>   parameter_map_buffer;
@@ -141,14 +144,14 @@ int main()
         auto message = read_parameter_map_queue.read(parameter_map_buffer);
         if (message.has_value())
         {
-            auto const json_manifest = vslib::utils::readJsonFromMessageQueue(message.value());
-            std::cout << json_manifest.dump(1) << "\n";
+            auto const json_manifest       = vslib::utils::readJsonFromMessageQueue(message.value());
+            // std::cout << json_manifest.dump(1) << "\n";
             auto const settable_parameters = parseManifest(json_manifest);
             commands                       = prepareCommands(settable_parameters);
             commands_set                   = true;
         }
 
-        if (commands_set && (commands_sent < commands.size()))
+        if (commands_set && (commands_sent <= commands.size()))
         {
             // check status of previous command
             auto message = read_command_status_queue.read(command_status_buffer);
@@ -161,12 +164,15 @@ int main()
             {
                 std::cout << "No status\n";
             }
-            std::cout << "Command sent: " << commands[commands_sent].dump() << std::endl;
-            vslib::utils::writeJsonToMessageQueue(commands[commands_sent], write_commands_queue);
-            commands_sent++;
+            if (commands_set < commands.size())
+            {
+                std::cout << "Command sent: " << commands[commands_sent].dump() << std::endl;
+                vslib::utils::writeJsonToMessageQueue(commands[commands_sent], write_commands_queue);
+                commands_sent++;
+            }
         }
 
-        if (commands_sent == commands.size())
+        if (commands_sent > commands.size())
         {
             break;
         }
@@ -174,7 +180,7 @@ int main()
         // END OF TEST CODE
 
         // Add some delay to simulate work
-        usleep(1'000'000);   // 1 s
+        usleep(100'000);   // 100 ms
     }
     std::cout << "Now running the console: \n";
     runConsoleUntilInterrupted(*domain);
