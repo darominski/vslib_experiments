2736d56a5e6cb1b7f01796206d49720689a93c76
Dominik Arominski <dominik.arominski@cern.ch>
2024-08-21 13:33:32 +0200

    AbcToDq0Transform: removed use of sin, cos lookup tables

--- source/vslib/components/inc/abcToDq0Transform.h
+++ source/vslib/components/inc/abcToDq0Transform.h
@@ -7,41 +7,33 @@
 #include <string>
 
 #include "component.h"
-#include "cosLookupTable.h"
-#include "sinLookupTable.h"
 
 namespace vslib
 {
     class AbcToDq0Transform : public Component
     {
       public:
-        //! Creates the AbcToDq0Transform Component with the LookupTables of the requested size.
+        //! Creates the AbcToDq0Transform Component.
         //!
         //! @param name Name of the Component
         //! @param parent Parent of this Component
         //! @param number_points Number of points for the sine and cosine lookup tables
-        AbcToDq0Transform(std::string_view name, IComponent& parent, const uint64_t number_points = 1000)
-            : Component("AbcToDq0Transform", name, parent),
-              m_sin("sin", *this, number_points),
-              m_cos("cos", *this, number_points)
+        AbcToDq0Transform(std::string_view name, IComponent& parent)
+            : Component("AbcToDq0Transform", name, parent)
         {
         }
 
         //! Performs a-b-c to d-q-0 transform from a three-phase (abc) to a dq0 rotating reference frame. Angular
-        //! position of the rotating frame is given by input theta. By default, the a-phase to q-axis alignment is
+        //! position of the rotating frame is given by input wt. By default, the a-phase to q-axis alignment is
         //! assumed but this can be modified with the manual offset parameter.
         //!
         //! @param a a-phase value
         //! @param b b-phase value
         //! @param c c-phase value
-        //! @param theta theta angle (in radians) between q and a
+        //! @param wt angle (in radians) between q and a
         //! @param offset Offset angle alignment (in radians): 0 is q alignment, pi/2 for d and a alignment
         //! @return Tuple of d, q, 0 values
         [[nodiscard]] std::tuple<double, double, double>
-        transform(const double a, const double b, const double c, const double theta, const double offset = 0) noexcept;
-
-      private:
-        SinLookupTable m_sin;   //!< Lookup table holding sine function
-        CosLookupTable m_cos;   //!< Lookup table holding cosine function
+        transform(const double a, const double b, const double c, const double wt, const double offset = 0) noexcept;
     };
 }   // namespace vslib
