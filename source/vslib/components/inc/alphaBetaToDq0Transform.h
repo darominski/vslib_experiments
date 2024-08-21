56f7084e725e375efc39e91be7c0021c23b20d2f
Dominik Arominski <dominik.arominski@cern.ch>
2024-08-21 13:35:27 +0200

    AlphaBetaToDq0Transform: removed use of sin and cos lookup tables

--- source/vslib/components/inc/alphaBetaToDq0Transform.h
+++ source/vslib/components/inc/alphaBetaToDq0Transform.h
@@ -20,10 +20,8 @@ namespace vslib
         //!
         //! @param name Name of the Component
         //! @param parent Parent of this Component
-        AlphaBetaToDq0Transform(std::string_view name, IComponent& parent, const uint64_t number_points = 1000)
-            : Component("AlphaBetaToDq0Transform", name, parent),
-              m_sin("sin", *this, number_points),
-              m_cos("cos", *this, number_points)
+        AlphaBetaToDq0Transform(std::string_view name, IComponent& parent)
+            : Component("AlphaBetaToDq0Transform", name, parent)
         {
         }
 
@@ -32,17 +30,12 @@ namespace vslib
         //! @param f_alpha alpha-component of alpha-beta-zero-frame component
         //! @param f_beta beta-component of alpha-beta-zero-frame component
         //! @param f_0 zero-component of alpha-beta-zero-frame component
-        //! @param theta theta angle (in radians) between q and alpha
+        //! @param wt angle (in radians) between q and alpha
         //! @param a_alignment Whether the frame alignment at t=0 is aligned with A-axis (true) or 90 degrees behind
         //! A-axis (false)
         //! @return Tuple of d, q, 0 values
         [[nodiscard]] std::tuple<double, double, double> transform(
-            const double f_alpha, const double f_beta, const double f_0, const double theta,
-            const bool a_alignment = true
+            const double f_alpha, const double f_beta, const double f_0, const double wt, const bool a_alignment = true
         ) noexcept;
-
-      private:
-        SinLookupTable m_sin;   //!< Lookup table holding sine function
-        CosLookupTable m_cos;   //!< Lookup table holding cosine function
     };
 }   // namespace vslib
