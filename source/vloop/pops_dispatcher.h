0ad50539744224da08e69f017c2b868a66ecfd27
Dominik Arominski <dominik.arominski@cern.ch>
2024-10-25 13:56:23 +0200

    WIP: split voltage dispatching into methods handling only recharching or cycle

--- source/vloop/pops_dispatcher.h
+++ source/vloop/pops_dispatcher.h
@@ -185,155 +185,24 @@ namespace user
 
         void dispatchVoltage(const double current_time, const double v_ref, const double i_mag_meas) noexcept
         {
-            const bool original_calculation = false;
-
-            // initialize the indices and kc, kf to zero:
-            double v_ref_1 = 0;
-            double v_ref_2 = 0;
-            double v_ref_3 = 0;
-            double v_ref_4 = 0;
-            double v_ref_5 = 0;
-            double v_ref_6 = 0;
-            double kc      = 0;
-            double kf      = 0;
-
-            const int    n_dcdc = getNdcdc(current_time);
-            const double v_r    = magnets_r * i_mag_meas;
-            const double v_l    = v_ref - v_r;
+            const double v_r = magnets_r * i_mag_meas;
+            const double v_l = v_ref - v_r;
 
             if (current_time < m_recharge_time)
             {
-                if (n_dcdc == 1)
-                {
-                    v_ref_1 = v_ref;
-                    // the rest remains unchanged
-                }
-                else if (n_dcdc == 2)
-                {
-                    v_ref_1 = 0.5 * v_ref;
-                    v_ref_2 = 0.5 * v_ref;
-                    // the rest remain unchanged
-                }
-                else   // n_dcdc == 6
-                {
-                    if (original_calculation)
-                    {
-                        // assuming original calculation:
-                        // energy needed to bring floaters to nominal voltage:
-                        const double Ef
-                            = m_n_floaters * m_k * (std::pow(m_Udc_max_chargers, 2) - std::pow(m_Udc_min_chargers, 2));
-                        // energy needed to bring chargers to nominal voltage:
-                        const double Ech
-                            = m_n_chargers * m_k * (std::pow(m_Udc_max_floaters, 2) - std::pow(m_Udc_min_floaters, 2));
-
-                        const double E = Ef + Ech;
-                        kf             = Ef / E;
-                        kc             = Ech / E;
-
-                        v_ref_1 = v_ref * (1 - kf) / m_n_chargers;
-                        v_ref_2 = v_ref_1;
-                        v_ref_3 = v_ref * kf / m_n_floaters;
-                        v_ref_4 = v_ref_3;
-                        v_ref_5 = v_ref_3;
-                        v_ref_6 = v_ref_3;
-                    }
-                    else
-                    {
-                        // fixed-factors approach
-                        if (fabs(v_l) < m_v_min)
-                        {
-                            v_ref_1 = v_ref * 0.3;
-                            v_ref_2 = v_ref_1;
-                            v_ref_3 = v_ref * 0.1;
-                            v_ref_4 = v_ref_3;
-                            v_ref_5 = v_ref_3;
-                            v_ref_6 = v_ref_3;
-                        }
-                        else
-                        {
-                            v_ref_1 = 0.5 * v_r + 0.1 * v_l;
-                            v_ref_2 = v_ref_1;
-                            v_ref_3 = 0.2 * v_l;
-                            v_ref_4 = v_ref_3;
-                            v_ref_5 = v_ref_3;
-                            v_ref_6 = v_ref_3;
-                        }
-                    }
-                }
+                const int n_dcdc = getNdcdc(current_time);
+                dispatchCycle(v_ref, v_r, v_l, n_dcdc);
             }
             else   // recharge is active
             {
-                const double nominal_v2 = std::pow(5000.0, 2);
-                for (int index = 0; index < m_dEc.size(); index++)
-                {
-                    const double energy = 0.5 * 0.247 * (nominal_v2 - std::pow(m_v_dc_meas[index], 2));
-                    m_dEc[index]        = (energy > 0) ? energy : 0.0;
-                }
-                // total energy required to charge chargers to nominal voltage:
-                double Ec = (m_dEc[0] + m_dEc[1]);
-                if (Ec < 0)   // could be negative if AFE is too fast
-                {
-                    Ec = 0;
-                }
-                // total energy required to charge floaters to nominal voltage:
-                const double Ef = m_dEc[2] + m_dEc[3] + m_dEc[4] + m_dEc[5];
-
-                if (Ef > 0 && i_mag_meas > 0)   // floaters not completely charged
-                {
-                    kf = 2.0 * Ef / (magnets_l * std::pow(i_mag_meas, 2));
-                    if (kf > 1.0)
-                    {
-                        kf = 1.0;
-                    }
-                    kc = 1.0 - kf;
-                }
-                else
-                {
-                    kf = 0;
-                    kc = 1;
-                }
-
-                if (kf > 0)
-                {
-                    // chargers:
-                    if (Ec > 0)
-                    {
-                        v_ref_1 = 0.5 * v_r + kc * v_l * (m_dEc[0] / Ec);
-                        v_ref_2 = 0.5 * v_r + kc * v_l * (m_dEc[1] / Ec);
-                    }
-                    else
-                    {
-                        v_ref_1 = 0.5 * (v_r + v_l * kc);
-                        v_ref_2 = 0.5 * (v_r + v_l * kc);
-                    }
-                    // floaters:
-                    v_ref_3 = v_l * kf * (m_dEc[2] / Ef);
-                    v_ref_4 = v_l * kf * (m_dEc[3] / Ef);
-                    v_ref_5 = v_l * kf * (m_dEc[4] / Ef);
-                    v_ref_6 = v_l * kf * (m_dEc[5] / Ef);
-                }
-                else
-                {
-                    // chargers:
-                    v_ref_1 = 0.5 * v_ref;
-                    v_ref_2 = 0.5 * v_ref;
-                    // floaters:
-                    v_ref_3 = 0.0;
-                    v_ref_4 = 0.0;
-                    v_ref_5 = 0.0;
-                    v_ref_6 = 0.0;
-                }
+                dispatchRecharging(v_ref, i_mag_meas, v_r, v_l);
             }
 
             // set the outputs:
-            m_idx[0] = v_ref_1 / m_v_dc_meas[0];
-            m_idx[1] = v_ref_2 / m_v_dc_meas[1];
-            m_idx[2] = v_ref_3 / m_v_dc_meas[2];
-            m_idx[3] = v_ref_4 / m_v_dc_meas[3];
-            m_idx[4] = v_ref_5 / m_v_dc_meas[4];
-            m_idx[5] = v_ref_6 / m_v_dc_meas[5];
-            m_kc     = kc;   // only calculated when recharging or n_dcdc is 6 and original calculation is used
-            m_kf     = kf;   // only calculated when recharging or n_dcdc is 6 and original calculation is used
+            for (int index = 0; index < TotalNumberDCDC; index++)
+            {
+                m_idx[index] = m_v_ref_dispatch[index] / m_v_dc_meas[index];
+            }
         }
 
         // ************************************************************
@@ -348,10 +217,11 @@ namespace user
         }
 
       private:
-        std::map<std::string_view, float>   m_cyclic_data;      //! P80 cyclic data from FGC2
-        std::array<double, TotalNumberDCDC> m_v_dc_meas{0.0};   //! Vdc measurements for each DCDC
-        std::array<double, TotalNumberDCDC> m_idx{0.0};         //! Modulation indices for each DCDC
-        std::array<double, TotalNumberDCDC> m_dEc{0};           //! Energy needed to recharge DCDC to nominal voltage
+        std::map<std::string_view, float>   m_cyclic_data;         //! P80 cyclic data from FGC2
+        std::array<double, TotalNumberDCDC> m_v_dc_meas{0.0};      //! Vdc measurements for each DCDC
+        std::array<double, TotalNumberDCDC> m_v_ref_dispatch{0};   //! Vref split between DCDCs
+        std::array<double, TotalNumberDCDC> m_idx{0.0};            //! Modulation indices for each DCDC
+        std::array<double, TotalNumberDCDC> m_dEc{0};              //! Energy needed to recharge DCDC to nominal voltage
 
         //! Parameters of the converter, will be fixed or provided by Configurator? Or VSlib GUI Parameter?
         const double m_v_min{35};              //!< Minimum voltage that IGBT can deliver, in V
@@ -372,6 +242,8 @@ namespace user
 
         int    m_current_plateau_id{0};
         double m_recharge_time{0.0};
+
+        bool m_original_calculation{false};   //! Whether to use the 'original' calculation or not
 
         //! Returns cycle duration, depending on the time of the last plateau.
         //!
@@ -450,6 +322,133 @@ namespace user
         double interpolateToNext(const double x, const double x1, const double y1, const double x2, const double y2)
         {
             return y1 + (x - x1) * (y2 - y1) / (x2 - x1);
+        }
+
+        void dispatchRecharging(const double v_ref, const double i_mag_meas, const double v_r, const double v_l)
+        {
+            std::fill(m_v_ref_dispatch.begin(), m_v_ref_dispatch.end(), 0.0);
+            double kc;
+            double kf;
+
+            const double nominal_v2 = std::pow(5000.0, 2);
+            for (int index = 0; index < m_dEc.size(); index++)
+            {
+                const double energy = 0.5 * 0.247 * (nominal_v2 - std::pow(m_v_dc_meas[index], 2));
+                m_dEc[index]        = (energy > 0) ? energy : 0.0;
+            }
+            // total energy required to charge chargers to nominal voltage:
+            double Ec = (m_dEc[0] + m_dEc[1]);
+            if (Ec < 0)   // could be negative if AFE is too fast
+            {
+                Ec = 0;
+            }
+            // total energy required to charge floaters to nominal voltage:
+            const double Ef = m_dEc[2] + m_dEc[3] + m_dEc[4] + m_dEc[5];
+
+            if (Ef > 0 && i_mag_meas > 0)   // floaters not completely charged
+            {
+                kf = 2.0 * Ef / (magnets_l * std::pow(i_mag_meas, 2));
+                if (kf > 1.0)
+                {
+                    kf = 1.0;
+                }
+                kc = 1.0 - kf;
+            }
+            else
+            {
+                kf = 0;
+                kc = 1;
+            }
+
+            if (kf > 0)
+            {
+                // chargers:
+                if (Ec > 0)
+                {
+                    m_v_ref_dispatch[0] = 0.5 * v_r + kc * v_l * (m_dEc[0] / Ec);
+                    m_v_ref_dispatch[1] = 0.5 * v_r + kc * v_l * (m_dEc[1] / Ec);
+                }
+                else
+                {
+                    m_v_ref_dispatch[0] = 0.5 * (v_r + v_l * kc);
+                    m_v_ref_dispatch[1] = 0.5 * (v_r + v_l * kc);
+                }
+                // floaters:
+                m_v_ref_dispatch[2] = v_l * kf * (m_dEc[2] / Ef);
+                m_v_ref_dispatch[3] = v_l * kf * (m_dEc[3] / Ef);
+                m_v_ref_dispatch[4] = v_l * kf * (m_dEc[4] / Ef);
+                m_v_ref_dispatch[5] = v_l * kf * (m_dEc[5] / Ef);
+            }
+            else
+            {
+                // chargers:
+                m_v_ref_dispatch[0] = 0.5 * v_ref;
+                m_v_ref_dispatch[1] = 0.5 * v_ref;
+                // floaters:
+                m_v_ref_dispatch[2] = 0.0;
+                m_v_ref_dispatch[3] = 0.0;
+                m_v_ref_dispatch[4] = 0.0;
+                m_v_ref_dispatch[5] = 0.0;
+            }
+        }
+
+        void dispatchCycle(const double v_ref, const double v_r, const double v_l, const int n_dcdc)
+        {
+            std::fill(m_v_ref_dispatch.begin(), m_v_ref_dispatch.end(), 0.0);
+
+            double kf;
+            double kc;
+            if (n_dcdc == 1)
+            {
+                m_v_ref_dispatch[0] = v_ref;
+                // the rest remains unchanged
+            }
+            else if (n_dcdc == 2)
+            {
+                m_v_ref_dispatch[0] = 0.5 * v_ref;
+                m_v_ref_dispatch[1] = 0.5 * v_ref;
+                // the rest remains unchanged
+            }
+            else   // n_dcdc == 6
+            {
+                if (m_original_calculation)
+                {
+                    // assuming original calculation:
+                    // energy needed to bring floaters to nominal voltage:
+                    const double Ef
+                        = m_n_floaters * m_k * (std::pow(m_Udc_max_chargers, 2) - std::pow(m_Udc_min_chargers, 2));
+                    // energy needed to bring chargers to nominal voltage:
+                    const double Ech
+                        = m_n_chargers * m_k * (std::pow(m_Udc_max_floaters, 2) - std::pow(m_Udc_min_floaters, 2));
+
+                    const double E = Ef + Ech;
+                    kf             = Ef / E;
+                    kc             = Ech / E;
+
+                    m_v_ref_dispatch[0] = v_ref * (1 - kf) / m_n_chargers;
+                    m_v_ref_dispatch[1] = m_v_ref_dispatch[0];
+                    m_v_ref_dispatch[5] = m_v_ref_dispatch[4] = m_v_ref_dispatch[3] = m_v_ref_dispatch[2]
+                        = v_ref * kf / m_n_floaters;
+                }
+                else
+                {
+                    // fixed-factors approach
+                    if (fabs(v_l) < m_v_min)
+                    {
+                        m_v_ref_dispatch[0] = v_ref * 0.3;
+                        m_v_ref_dispatch[1] = m_v_ref_dispatch[0];
+                        m_v_ref_dispatch[5] = m_v_ref_dispatch[4] = m_v_ref_dispatch[3] = m_v_ref_dispatch[2]
+                            = v_ref * 0.1;
+                    }
+                    else
+                    {
+                        m_v_ref_dispatch[0] = 0.5 * v_r + 0.1 * v_l;
+                        m_v_ref_dispatch[1] = m_v_ref_dispatch[0];
+                        m_v_ref_dispatch[5] = m_v_ref_dispatch[4] = m_v_ref_dispatch[3] = m_v_ref_dispatch[2]
+                            = 0.2 * v_l;
+                    }
+                }
+            }
         }
     };
 }
