2a74469a22a3a1542719f5830c2ef9fbc00ec1df
Dominik Arominski <dominik.arominski@cern.ch>
2025-01-15 17:40:46 +0100

    Mark unused functions as 'maybe_unused'

--- source/utils/pollCpuClock.h
+++ source/utils/pollCpuClock.h
@@ -8,13 +8,11 @@
 
 namespace fgc4::utils
 {
-
-    static uint64_t read_CNTPCT()
+    [[maybe_unused]] static uint64_t read_CNTPCT()
     {
         uint64_t cntval;
         // asm statement MUST be volatile, otherwise compiler will do weird, wrong things like coalescing the access
         asm volatile("mrs %0, CNTPCT_EL0" : "=r"(cntval));
         return cntval;
     }
-
 }
