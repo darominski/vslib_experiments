30f5cd8d969092f962cab38dfb7cc8eeedb2960d
Dominik Arominski <dominik.arominski@cern.ch>
2025-01-24 10:52:04 +0100

    FullBridge: added doxygen comments, moved switching polarity to private, removed duty cycle steering from more complex cases

--- source/vslib/components/inc/fullBridge.h
+++ source/vslib/components/inc/fullBridge.h
@@ -105,43 +105,24 @@ namespace vslib
             leg_2.setModulationIndex(-modulation_index);
         }
 
+        //! Sets the duty cycle of positive unipolar two-level full bridge.
+        //!
+        //! @param duty_cycle Duty cycle of the full bridge, will be set to leg 2
         void setDutyCyclePositive(const float duty_cycle) noexcept
         {
             leg_1.setDutyCycle(1.0);
             leg_2.setDutyCycle(duty_cycle);
         }
 
+
+        //! Sets the duty cycle of negative unipolar two-level full bridge.
+        //!
+        //! @param duty_cycle Duty cycle of the full bridge, will be set to leg 1
         void setDutyCycleNegative(const float duty_cycle) noexcept
         {
             leg_1.setDutyCycle(duty_cycle);
             leg_2.setDutyCycle(1.0);
         }
-
-        void setDutyCycle2L1Fsw(const float duty_cycle) noexcept
-        {
-            if (!m_bipolar)
-            {
-                switchBipolar(true);
-            }
-            //???
-        }
-
-        void setDutyCycle3L2Fsw(const float duty_cycle) noexcept
-        {
-        }
-
-        void switchBipolar(const bool setting) noexcept
-        {
-            m_bipolar = setting;
-            leg_2.invertA(setting);
-            leg_2.invertB(!setting);
-        }
-
-        // ************************************************************
-        // Settable Parameters
-
-        // ************************************************************
-        // Owned Components
 
         std::optional<fgc4::utils::Warning> verifyParameters() override
         {
@@ -152,6 +133,19 @@ namespace vslib
         HalfBridge leg_1;   //!< Leg 1 of the Full Bridge
         HalfBridge leg_2;   //!< Leg 2 of the FUll Bridge
 
-        bool m_bipolar;   //!< Flag informing whether the full bridge is running in bipolar mode
+        //! Flag informing whether the full bridge is running in bipolar mode
+        bool m_bipolar{false};
+        // ************************************************************
+
+        //! Switches the bipolar mode according to the setting. Inverts the PWMA and PWMB signals.
+        //!
+        //! @param setting If true, the bipolar mode is enabled and leg 2's PWMA and PWMB are inverted, false otherwise
+        //! (default)
+        void switchBipolar(const bool setting) noexcept
+        {
+            m_bipolar = setting;
+            leg_2.invertA(setting);
+            leg_2.invertB(!setting);
+        }
     };
 }
