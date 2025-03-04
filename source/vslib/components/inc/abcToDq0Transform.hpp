0670a92fbc745e7c4ad86f5d2d42181d6678e576
Dominik Arominski <dominik.arominski@cern.ch>
2025-03-04 11:22:46 +0100

    Style: AbcToDq0Transform: increased length of parameters, private members are prefixed with m_

--- source/vslib/components/inc/abcToDq0Transform.hpp
+++ source/vslib/components/inc/abcToDq0Transform.hpp
@@ -22,8 +22,8 @@ namespace vslib
         //! @param number_points Number of points for the sine and cosine lookup tables
         AbcToDq0Transform(std::string_view name, Component& parent, const uint64_t number_points = 10'000)
             : Component("AbcToDq0Transform", name, parent),
-              abc_to_alphabeta("abc_to_alphabeta", *this),
-              alphabeta_to_dq0("alphabeta_to_dq0", *this, number_points)
+              m_abc_to_alphabeta("abc_to_alphabeta", *this),
+              m_alphabeta_to_dq0("alphabeta_to_dq0", *this, number_points)
         {
         }
 
@@ -31,17 +31,18 @@ namespace vslib
         //! position of the rotating frame is given by input wt. By default, the a-phase to q-axis alignment is
         //! assumed but this can be modified with the manual offset parameter.
         //!
-        //! @param a a-phase value
-        //! @param b b-phase value
-        //! @param c c-phase value
+        //! @param f_a a-phase value
+        //! @param f_b b-phase value
+        //! @param f_c c-phase value
         //! @param wt angle (in radians) between q and a
         //! @param offset Offset angle alignment (in radians): 0 is q alignment, pi/2 for d and a alignment
         //! @return Tuple of d, q, 0 values
-        [[nodiscard]] std::tuple<double, double, double>
-        transform(const double a, const double b, const double c, const double wt, const double offset = 0) noexcept;
+        [[nodiscard]] std::tuple<double, double, double> transform(
+            const double f_a, const double f_b, const double f_c, const double wt, const double offset = 0
+        ) noexcept;
 
       private:
-        AbcToAlphaBetaTransform abc_to_alphabeta;
-        AlphaBetaToDq0Transform alphabeta_to_dq0;
+        AbcToAlphaBetaTransform m_abc_to_alphabeta;
+        AlphaBetaToDq0Transform m_alphabeta_to_dq0;
     };
 }   // namespace vslib
