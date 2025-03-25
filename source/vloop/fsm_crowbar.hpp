aadeba228f84d70e2a0f14eebcf8b9e4fdd4dff7
Dominik Arominski <dominik.arominski@cern.ch>
2025-03-25 17:47:58 +0100

    Skeleton structure for DCDC charging and DCDC floating

---
+++ source/vloop/fsm_crowbar.hpp
@@ -0,0 +1,82 @@
+//! @file
+//! @brief Definition of Finite state machine for a crowbar
+//! @author Dominik Arominski
+
+#pragma once
+
+#include "constants.hpp"
+#include "fsm.hpp"
+
+namespace user
+{
+    enum class CrowbarStates
+    {
+        fault_off,
+        on
+    };
+
+    class CrowbarFSM
+    {
+        using StateMachine = ::utils::Fsm<CrowbarStates, CrowbarFSM, false>;
+
+        using TransRes = ::utils::FsmTransitionResult<CrowbarStates>;
+
+        using StateFunc = std::function<void(void)>;
+
+        //! Convenience alias representing pointer to a member function of the Parent class, for a transition function.
+        using TransitionFunc = ::utils::FsmTransitionResult<CrowbarStates> (CrowbarFSM::*)();
+
+      public:
+        CrowbarFSM()
+            : m_fsm(*this, CrowbarStates::fault_off)
+        {
+            // CAUTION: The order of transition method matters
+
+            // clang-format off
+            m_fsm.addState(CrowbarStates::fault_off, &CrowbarFSM::onFaultOff, {&CrowbarFSM::toOn});
+            m_fsm.addState(CrowbarStates::on,        &CrowbarFSM::onOn,       {&CrowbarFSM::toFaultOff});
+            // clang-format on
+        }
+
+        void update()
+        {
+            m_fsm.update();
+        }
+
+        [[nodiscard]] const auto getState() const noexcept
+        {
+            return m_fsm.getState();
+        }
+
+      private:
+        StateMachine m_fsm;
+
+        void onFaultOff()
+        {
+            // open the safety chain?
+        }
+
+        void onOn()
+        {
+        }
+
+        TransRes toOn()
+        {
+            if (I_loop.getState() == VS_RUN)
+            {
+                return TransRes{CrowbarStates::on};
+            }
+            return {};
+        }
+
+        TransRes toFaultOff()
+        {
+            if (!intertrip_light || I_loop.getState() == FO)
+            {
+                {CrowbarStates::fault_off};
+            }
+            return {};
+        }
+    };
+
+}   // namespace user
