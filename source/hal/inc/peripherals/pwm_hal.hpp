ca165fdc851ba3f5feb2dbbd3a6a273e07d27c63
Dominik Arominski <dominik.arominski@cern.ch>
2025-04-15 14:49:52 +0200

    Updated PWM to reflect changes in the cheby export

--- source/hal/inc/peripherals/pwm_hal.hpp
+++ source/hal/inc/peripherals/pwm_hal.hpp
@@ -4,28 +4,29 @@
 
 #pragma once
 
-#include "cheby_gen/pwm_regs.h"
+#include "cheby_gen/mb_top.hpp"
 
 namespace hal
 {
 
+    template<uint32_t pwm_id>
     class PWM
     {
       public:
-        typedef myModule::PwmRegs::LoadMode UpdateType;
+        typedef unnamed::Top::PwmArrayItem::Pwm::UpdateType UpdateType;
 
         //! Constructor for PWM HAL object.
-        //!
-        //! @param base_address Base address of the PWM IP, to be replaced by memory mapping.
-        PWM(uint8_t* base_address)
-        noexcept
-            : m_regs(base_address)
+        PWM() noexcept
         {
+            static_assert(pwm_id < 12, "The PWM ID must be between 0 and 11.");
+            unnamed::Top top(reinterpret_cast<uint8_t*>(0xa0000000));
+            m_regs = top.pwm[pwm_id].pwm;
+
             // Assumption is made t hat the configuration does not change at runtime, so configuration can be
             // internalized in memory rather than always read from register
-            m_max_counter_value = m_regs.ctrh.read();
+            m_max_counter_value = m_regs.ctrhSc.read();
 
-            const uint32_t dead_time = m_regs.deadtime.read();   // dead time, in clock cycles
+            const uint32_t dead_time = m_regs.deadtimeSc.read();   // dead time, in clock cycles
             const uint32_t min_switch_time
                 = m_regs.minSwitchTimeSc.read();   // minimum ON or OFF switch time, in clock cycles
             // max counter value serves as period in clock cycles
@@ -34,14 +35,18 @@ namespace hal
                 = 1.0 - static_cast<float>((m_max_counter_value - min_switch_time - dead_time) / m_max_counter_value);
             m_max_modulation_index
                 = static_cast<float>(m_max_counter_value - min_switch_time - dead_time) / (m_max_counter_value);
+
+            // TMP: Configurator will eventually configure this IP core:
         }
 
         //! Sets the desired modulation index.
         //!
         //! @param index Modulation index to be used, limited from 0 to 1 (at maximum)
+        //! @param write_to_cc0 Flag to decide whether to write to CC0 (symmetrical case), or to CC1 (assymetry between
+        //! CC0 and CC1)
         //! @return Boolean value with information whether the modulation index value set is identical to the one
         //! provided (true), false otherwise
-        bool setModulationIndex(const float modulation_index) noexcept
+        bool setModulationIndex(const float modulation_index, bool write_to_cc0 = true) noexcept
         {
             const float index     = forceLimit(modulation_index, m_min_modulation_index, m_max_modulation_index);
             const float threshold = getMaximumCounterValue() * index;
@@ -50,20 +55,27 @@ namespace hal
                 // avoid setting NaN to register, return early
                 return false;
             }
-            writeTriggerValue(static_cast<uint32_t>(threshold));
+            if (write_to_cc0)
+            {
+                writeCC0(static_cast<uint32_t>(threshold));
+            }
+            else
+            {
+                writeCC1(static_cast<uint32_t>(threshold));
+            }
             return (index == modulation_index);
         }
 
         //! Configures the PWM to stay high.
         void setHigh() noexcept
         {
-            writeTriggerValue(0);
+            writeCC0(0);
         }
 
         //! Configures the PWM to stay low.
         void setLow() noexcept
         {
-            writeTriggerValue(getMaximumCounterValue());
+            writeCC0(getMaximumCounterValue());
         }
 
         //! Sets the update type.
@@ -71,7 +83,7 @@ namespace hal
         //! @param type Update type to be set, one of zero, prd, zeroPrd, immediate
         void setUpdateType(const UpdateType type) noexcept
         {
-            m_regs.conf.loadMode.set(type);
+            m_regs.config.updateType.set(type);
         }
 
         //! Sets the additional extended dead time.
@@ -79,7 +91,7 @@ namespace hal
         //! @param additional_dead_time Additional dead time, in clock ticks
         void setExtendedDeadTime(const uint32_t additional_dead_time) noexcept
         {
-            m_regs.extDeadtime.write(additional_dead_time);
+            m_regs.extendedDeadtimeSc.write(additional_dead_time);
         }
 
         //! Sets the enabled status for PWMA.
@@ -87,7 +99,7 @@ namespace hal
         //! @param setting Flag for setting the PWMA enabled status: enabled if true, disabled otherwise
         void setEnabledA(const bool setting) noexcept
         {
-            m_regs.dtctrl.disableA.set(!setting);
+            m_regs.config.disableA.set(!setting);
         }
 
         //! Sets the enabled status for PWMB.
@@ -95,7 +107,7 @@ namespace hal
         //! @param setting Flag for setting the PWMB enabled status: enabled if true, disabled otherwise
         void setEnabledB(const bool setting) noexcept
         {
-            m_regs.dtctrl.disableB.set(!setting);
+            m_regs.config.disableB.set(!setting);
         }
 
         //! Enables the PWM counters.
@@ -103,16 +115,25 @@ namespace hal
         //! @param setting Setting for the counters,
         void setEnabled(const bool setting) noexcept
         {
-            m_regs.ctrl.en.set(setting);
+            m_regs.ctrl.enable.set(setting);
         }
 
-        //! Sets the inverted status of PWMA.
+        //! Sets the inverted status of PWMA and PWMB.
         //!
-        //! @param setting Flag for setting the PWMA inverted status: inverted if true, not inverted otherwise
-        void setInverted(bool setting) noexcept
+        //! @param setting Flag for setting the PWM inverted status: inverted if true, not inverted otherwise
+        void setInverted(const bool setting) noexcept
         {
-            m_regs.dtctrl.inva.set(setting);
-            m_regs.dtctrl.invb.set(setting);
+            m_regs.config.invert.set(setting);
+        }
+
+        //! Sets decoupling CC1 from CC0 to be used independently, e.g. in an inverter.
+        //!
+        //! @param setting Flag for setting the CC1 independently from CC0 (true), false if coupling is desired
+        //! (default).
+        void setDecoupleCC1(const bool setting) noexcept
+        {
+            symmetrical = !setting;
+            m_regs.config.decoupleCc1.set(setting);
         }
 
         //! Reads the maximum counter value.
@@ -123,12 +144,20 @@ namespace hal
             return m_max_counter_value;
         }
 
-        //! Reads the modulation index value.
+        //! Reads the modulation index value (CC0).
         //!
-        //! @return Modulation index set to CC
+        //! @return Modulation index set to CC0
         uint32_t getModulationIndex() const noexcept
         {
-            return m_regs.cc.read();
+            return m_regs.cc0Sc.read();
+        }
+
+        //! Reads the modulation index value (CC1).
+        //!
+        //! @return Modulation index set to CC1
+        uint32_t getModulationIndexCC1() const noexcept
+        {
+            return m_regs.carrierBits.read();
         }
 
         //! Returns the constant size of PWM
@@ -136,11 +165,11 @@ namespace hal
         //! @return Maximum size of the PWM registers
         static auto constexpr size() noexcept
         {
-            return myModule::PwmRegs::csize();
+            return unnamed::Top::PwmArrayItem::size;
         }
 
       private:
-        myModule::PwmRegs m_regs;
+        unnamed::Top::PwmArrayItem::Pwm m_regs;
 
         //! Maximum counter value to which the PWM counter is counting to, a configuration parameter.
         uint32_t m_max_counter_value{0};
@@ -149,12 +178,23 @@ namespace hal
         //! Maximum modulation index that can be set for this particular PWM.
         float m_max_modulation_index{1.0};
 
-        //! Writes to the PWM trigger register, allows to set the trigger for PWM to go high.
+        //! Flag to signal whether CC0 and CC1 are equal
+        bool symmetrical{true};
+
+        //! Writes to the PWM trigger register CC0, allows to set the trigger for PWM to go high.
         //!
         //! @param threshold Threshold, in clock ticks, for PWM to go high
-        void writeTriggerValue(uint32_t threshold) noexcept
+        void writeCC0(uint32_t threshold) noexcept
         {
-            m_regs.cc.write(threshold);
+            m_regs.cc0Sc.write(threshold);
+        }
+
+        //! Writes to the PWM trigger register CC1, allows to set the trigger for PWM to go high.
+        //!
+        //! @param threshold Threshold, in clock ticks, for PWM to go high
+        void writeCC1(uint32_t threshold) noexcept
+        {
+            m_regs.cc1Sc.write(threshold);
         }
 
         //! Utility method forcing the provided value to fit in the specified limits min and max.
