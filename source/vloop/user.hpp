d3e45b26eed761e9b9d99b045f42084678b05f13
Dominik Arominski <dominik.arominski@cern.ch>
2025-06-27 17:03:34 +0200

    SVC 1 ADC readout test

--- source/vloop/user.hpp
+++ source/vloop/user.hpp
@@ -34,32 +34,9 @@ namespace user
               adc_5(),
               adc_6(),
               ad7606c_1(spi_1, 3, adc_1)
-        //   pwm_0("pwm_0", *this, 10'000),
-        //   pwm_1("pwm_1", *this, 10'000),
-        //   pwm_5("pwm_5", *this, 10'000),
-        //   pwm_6("pwm_6", *this, 10'000),
-        //   pwm_7("pwm_7", *this, 10'000),
-        //   pwm_8("pwm_8", *this, 10'000),
-        //   pwm_11("pwm_11", *this, 10'000),
-        //   full_bridge_1("fb_1", *this, 10'000),
-        //   sync_trig_arr(hal::Top::instance().syncTrig),
-        //   sync_time_ip(hal::Top::instance().syncTime)
         {
             // initialize all your objects that need initializing
-
-            // sync_trig_arr = hal::Top::instance().syncTrig;
-            // sync_time_ip = hal::Top::instance().syncTime;
-
-            // configure all STGs
-            // for (int index = 0; index < 27; index++)
-            // {
-            //     sync_trig_arr[index].stg.periodSc.write(2);
-            //     sync_trig_arr[index].stg.ctrl.resync.set(true);
-            // }
-            // sync_trig_arr[22].stg.delaySc.write(5'000);
-            // sync_trig_arr[23].stg.delaySc.write(5'000);
-            // sync_trig_arr[15+7].stg.delaySc.write(5'000);
-            std::cout << "Initialized\n";
+            std::cout << "Converter initialized\n";
         }
 
         // Define your public Components here
@@ -73,71 +50,7 @@ namespace user
 
         void init() override
         {
-            // m_s2rcpp.ctrl.pmaInit.set(false);
-            // sleep(2);
-
-            // m_s2rcpp.ctrl.resetPb.set(false);
-            // sleep(1);
-
-            // m_s2rcpp.ctrl.selOutput.set(true);
-
-            // if (!(m_s2rcpp.status.channelUp.get() && m_s2rcpp.status.gtPllLock.get() && m_s2rcpp.status.laneUp.get()
-            //       && m_s2rcpp.status.pllLocked.get() && m_s2rcpp.status.gtPowergood.get()))
-            // {
-            //     printf("Unexpected status: 0x%#08x\n", m_s2rcpp.ctrl.read());
-            // }
-
-            // if (m_s2rcpp.status.linkReset.get() || m_s2rcpp.status.sysReset.get())
-            // {
-            //     printf("Link is in reset\n");
-            // }
-
-            // if (m_s2rcpp.status.softErr.get() || m_s2rcpp.status.hardErr.get())
-            // {
-            //     printf("Got an error\n");
-            // }
-
-            // printf("Link up and good. Ready to receive data.\n");
-
-            // // kria transfer rate: 100us
-            // m_r2scpp.numData.write(num_data * 2);
-            // m_r2scpp.tkeep.write(0x0000FFFF);
-
-            // configure and start SyncTime
-            // std::chrono::time_point currently = std::chrono::time_point_cast<std::chrono::milliseconds>(
-            // std::chrono::system_clock::now()
-            // );
-            // std::chrono::duration miliseconds_since_utc_epoch = currently.time_since_epoch();
-
-            // const auto now = std::chrono::system_clock::now();
-            // const auto now_p_5s = now + std::chrono::seconds(5);
-            // const auto epoch_seconds =
-            // std::chrono::duration_cast<std::chrono::seconds>(now_p_5s.time_since_epoch()).count();
-
-            // pwm_0.start();
-            // pwm_1.start();
-            // pwm_5.start();
-            // pwm_11.start();
-            // pwm_6.start();
-            // pwm_7.start();
-            // pwm_8.start();
-
-            // full_bridge_1.start();
-
-            // const uint32_t current_time_w_offset = epoch_seconds; // 5 s in the future
-            // const uint32_t current_time_w_offset    = 0;   // 5 s in the future
-            // // const uint32_t current_time_w_offset = miliseconds_since_utc_epoch.count()*1000 + 5; // 5 s in the
-            // future const uint32_t current_time_w_offset_sc = 0;
-
-            // sync_time_ip.s.write(current_time_w_offset);
-            // sync_time_ip.sc.write(current_time_w_offset_sc);
-
-            // std::cout << "current: " << epoch_seconds  << " " << current_time_w_offset<< std::endl;
-            // std::cout << "set: " << sync_time_ip.s.read() << std::endl;
-
-            adc_1.start();
-            sleep(1);
-            std::cout << "init finished\n";
+            std::cout << "Init finished\n";
             interrupt_1.start();
         }
 
@@ -181,51 +94,7 @@ namespace user
         {
             converter.adc_1.start();
             std::cout << converter.adc_1.readConverted(1) << "\n";
-            // const auto success0 = converter.pwm_0.setModulationIndex(static_cast<float>(converter.counter) / 10'000);
-            // const auto success1 = converter.pwm_1.setModulationIndex(static_cast<float>(converter.counter) / 10'000);
-            // const auto success5 = converter.pwm_5.setModulationIndex(static_cast<float>(converter.counter) / 10'000);
-            // const auto success11 = converter.pwm_11.setModulationIndex(static_cast<float>(converter.counter) /
-            // 10'000);
-            // const auto success6 = converter.pwm_6.setModulationIndex(static_cast<float>(converter.counter) / 10'000);
-            // const auto success7 = converter.pwm_7.setModulationIndex(static_cast<float>(converter.counter) / 10'000);
-            // const auto success8 = converter.pwm_8.setModulationIndex(static_cast<float>(converter.counter) / 10'000);
-
-            // converter.full_bridge_1.setModulationIndex2L1Fsw(static_cast<float>(converter.counter) / 10'000);
-            // converter.full_bridge_1.setModulationIndex3L2Fsw(static_cast<float>(converter.counter - 5'000) / 10'000);
-
-            // if (converter.counter % 100 == 0)
-            // {
-            // std::cout << std::boolalpha << converter.counter << " " << success6 << '\n';
-            // }
-
-            // if (converter.count_up)
-            // {
-            //     converter.counter++;
-            // }
-            // else
-            // {
-            //     converter.counter--;
-            // }
-
-            // if (converter.counter >= 10'000)
-            // {
-            //     converter.count_up = false;
-            // }
-            // if (converter.counter <= 0)
-            // {
-            //     converter.count_up = true;
-            // }
         }
-
-        // vslib::HalfBridge<0> pwm_0;
-        // vslib::HalfBridge<1> pwm_1;
-        // vslib::HalfBridge<5> pwm_5;
-        // vslib::HalfBridge<6> pwm_6;
-        // vslib::HalfBridge<7> pwm_7;
-        // vslib::HalfBridge<8> pwm_8;
-        // vslib::HalfBridge<11> pwm_11;
-
-        // vslib::FullBridge<6> full_bridge_1;
 
         hal::Bus                bus_1;
         hal::XilAxiSpi          spi_1;
@@ -236,18 +105,6 @@ namespace user
         hal::UncalibratedADC<4> adc_5;
         hal::UncalibratedADC<5> adc_6;
         hal::AD7606C<0>         ad7606c_1;
-
-        // ipCores::Top::SyncTrigArray sync_trig_arr;
-        // ipCores::Top::SyncTime      sync_time_ip;
-
-      private:
-        int counter{0};
-        // bool count_up{true};
-        // constexpr static uint32_t    num_data{20};
-        // std::array<double, num_data> m_data;
-
-        // ipCores::StreamToReg m_s2rcpp;
-        // ipCores::RegToStream m_r2scpp;
     };
 
 }   // namespace user
