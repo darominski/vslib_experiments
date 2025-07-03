c987c6e5a8e8ea027954f94ac2eb6f790313c1b2
Dominik Arominski <dominik.arominski@cern.ch>
2025-07-03 18:34:07 +0200

    WIP: test with defining adc_array

--- source/vloop/user.hpp
+++ source/vloop/user.hpp
@@ -10,6 +10,19 @@
 
 namespace user
 {
+
+    template<typename T, std::size_t... Is>
+    constexpr std::array<T, sizeof...(Is)> make_array(std::index_sequence<Is...>)
+    {
+        return {{T(static_cast<int>(Is))...}};
+    }
+
+    template<typename T, std::size_t N>
+    constexpr std::array<T, N> make_array()
+    {
+        return make_array<T>(std::make_index_sequence<N>{});
+    }
+
     class Converter : public vslib::IConverter
     {
         static constexpr int data_queue_size = fgc4::utils::constants::json_memory_pool_size;   // 1 MB
@@ -23,13 +36,16 @@ namespace user
               )},
               bus_1(0xA0000000, pow(2, 24)),
               spi_1(bus_1, 0xE400),
-              adc_1(0),
-              adc_2(1),
-              adc_3(2),
-              adc_4(3),
-              adc_5(4),
-              adc_6(5),
-              ad7606c_1(spi_1, 3, adc_1)
+              //   adc_1(0),
+              //   adc_2(1),
+              //   adc_3(2),
+              //   adc_4(3),
+              //   adc_5(4),
+              //   adc_6(5),
+              adc_array(make_array<hal::UncalibratedADC, 24>()),
+              ad7606c_1(spi_1, 3, adc_array[0]),
+              ad7606c_2(spi_1, 4, adc_array[1]),
+              ad7606c_3(spi_1, 5, adc_array[2])
         {
             // initialize all your objects that need initializing
             std::cout << "Converter initialized\n";
@@ -37,6 +53,21 @@ namespace user
 
         // Define your public Components here
         vslib::TimerInterrupt<Converter> interrupt_1;
+
+        std::array<hal::UncalibratedADC, 24> adc_array;
+
+        hal::Bus       bus_1;
+        hal::XilAxiSpi spi_1;
+        // hal::UncalibratedADC adc_1;
+        // hal::UncalibratedADC adc_2;
+        // hal::UncalibratedADC adc_3;
+        // hal::UncalibratedADC adc_4;
+        // hal::UncalibratedADC adc_5;
+        // hal::UncalibratedADC adc_6;
+        hal::AD7606C ad7606c_1;
+        hal::AD7606C ad7606c_2;
+        hal::AD7606C ad7606c_3;
+
         // ...
         // end of your Components
 
@@ -88,30 +119,20 @@ namespace user
 
         static void RTTask(Converter& converter)
         {
-            converter.adc_1.start();
-            const auto value = converter.adc_1.readConverted(1);
+            converter.adc_array[0].start();
+            const auto value = converter.adc_array[0].readConverted(1);
             converter.data_queue.write(std::span<const uint8_t>{reinterpret_cast<const uint8_t*>(&value), sizeof(value)}
             );
 
             if (converter.counter++ % 1000)
             {
-                std::cout << converter.adc_1.readConverted(1) << "\n";
+                std::cout << converter.adc_array[0].readConverted(1) << "\n";
             }
         }
 
         int counter{0};
 
         fgc4::utils::MessageQueueWriter<void> data_queue;
-
-        hal::Bus                bus_1;
-        hal::XilAxiSpi          spi_1;
-        hal::UncalibratedADC<0> adc_1;
-        hal::UncalibratedADC<1> adc_2;
-        hal::UncalibratedADC<2> adc_3;
-        hal::UncalibratedADC<3> adc_4;
-        hal::UncalibratedADC<4> adc_5;
-        hal::UncalibratedADC<5> adc_6;
-        hal::AD7606C<0>         ad7606c_1;
     };
 
 }   // namespace user
