f0738e17f47fb774d06cf031d0723684b4374fd9
Dominik Arominski <dominik.arominski@cern.ch>
2025-08-06 14:29:50 +0200

    Bmboot notifyPayloadStarted method should be called as soon as possible when payload starts

--- source/vslib/main.cpp
+++ source/vslib/main.cpp
@@ -8,7 +8,7 @@ using namespace fgc4;
 
 int main()
 {
-    // bmboot::notifyPayloadStarted();
+    bmboot::notifyPayloadStarted();
     RootComponent& root = getRootComponent();
 
     // VSlib-side initialization:
